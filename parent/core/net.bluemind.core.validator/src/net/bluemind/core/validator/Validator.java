75d538ff249152edbf103e8a41d68eef3d8cf571
Anthony Prades <anthony.prades@bluemind.net>
2019-07-10 11:35:23 +0200

    BM-15041 Fix: load sanitizer and validor with priority

--- parent/core/net.bluemind.core.validator/src/net/bluemind/core/validator/Validator.java
+++ parent/core/net.bluemind.core.validator/src/net/bluemind/core/validator/Validator.java
@@ -29,19 +29,17 @@ import net.bluemind.core.rest.BmContext;
 import net.bluemind.eclipse.common.RunnableExtensionLoader;
 
 public class Validator {
-
 	private static final Logger logger = LoggerFactory.getLogger(Validator.class);
 
 	private static final List<IValidatorFactory<Object>> validators = loadValidator();
 
 	private static List<IValidatorFactory<Object>> loadValidator() {
 		RunnableExtensionLoader<IValidatorFactory<Object>> rel = new RunnableExtensionLoader<IValidatorFactory<Object>>();
-		List<IValidatorFactory<Object>> stores = rel.loadExtensions("net.bluemind.core", "validatorfactory",
+		List<IValidatorFactory<Object>> stores = rel.loadExtensionsWithPriority("net.bluemind.core", "validatorfactory",
 				"validatorfactory", "implementation");
 
-		for (IValidatorFactory<Object> j : stores) {
-			logger.info("validator factory class: {} for: {}", j.getClass().getName(), j.support());
-		}
+		stores.stream().forEach(store -> logger.info("validator factory class: {} for: {}", store.getClass().getName(),
+				store.support()));
 
 		return stores;
 	}
