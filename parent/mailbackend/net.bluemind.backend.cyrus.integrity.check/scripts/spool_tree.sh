d773aa202db61af0381c3bbbf1282ae29422658a
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2019-10-02 11:40:51 +0200

    Chore: fix find options order to prevent warning

--- parent/mailbackend/net.bluemind.backend.cyrus.integrity.check/scripts/spool_tree.sh
+++ parent/mailbackend/net.bluemind.backend.cyrus.integrity.check/scripts/spool_tree.sh
@@ -4,7 +4,7 @@ test -d /var/spool/cyrus/meta || exit 1
 
 # leaf directories: https://stackoverflow.com/a/50548890/909674
 cd /var/spool/cyrus/meta && \
-    find . -type d -maxdepth 7 | cut -b3- | \
+    find . -maxdepth 7 -type d | cut -b3- | \
     sort -r | awk 'a!~"^"$0{a=$0;print}' | sort
 
 exit 0
