4412f0a24e4a231a4ef9ff48740d2a3da8c4968f
kladier <kevin.ladier@bluemind.net>
2019-10-09 14:22:53 +0200

    FEATWEBML-472 Feat: merge styleguide i18n keys with common keys

--- ui/commons/net.bluemind.l10n.js/index.js
+++ ui/commons/net.bluemind.l10n.js/index.js
@@ -1,3 +1,5 @@
 import { TranslationHelper } from '@bluemind/i18n';
+import { StyleguideL10N } from '@bluemind/styleguide';
 
-export default  TranslationHelper.loadTranslations(require.context('./l10n', false, /\.json$/));
+const CommonL10N = TranslationHelper.loadTranslations(require.context('./l10n', false, /\.json$/));
+export default TranslationHelper.mergeTranslations(CommonL10N, StyleguideL10N);
