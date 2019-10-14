297c84c75713ff8510e5850e38e95ee385c24451
Thomas Fricker <thomas.fricker@blue-mind.net>
2019-10-14 16:04:28 +0200

    BM-15257 Fix: fix HTTP method of searchItems

--- parent/mailbackend/net.bluemind.backend.mail.api/src/net/bluemind/backend/mail/api/IBaseMailboxFolders.java
+++ parent/mailbackend/net.bluemind.backend.mail.api/src/net/bluemind/backend/mail/api/IBaseMailboxFolders.java
@@ -20,6 +20,7 @@ package net.bluemind.backend.mail.api;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -42,7 +43,7 @@ public interface IBaseMailboxFolders extends IChangelogSupport {
 	@Path("_all")
 	List<ItemValue<MailboxFolder>> all();
 
-	@GET
+	@POST
 	@Path("_search")
 	public SearchResult searchItems(MailboxFolderSearchQuery query);
 
