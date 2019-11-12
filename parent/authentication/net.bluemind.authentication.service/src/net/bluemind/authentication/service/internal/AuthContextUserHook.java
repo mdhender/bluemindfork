9384f129f961894a9715abe586567e0890c32442
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2019-11-12 15:46:38 +0100

    BM-15340 Fix: invalid all past, present aliases to deal with users having a new alias & logging-in with that

--- parent/authentication/net.bluemind.authentication.service/src/net/bluemind/authentication/service/internal/AuthContextUserHook.java
+++ parent/authentication/net.bluemind.authentication.service/src/net/bluemind/authentication/service/internal/AuthContextUserHook.java
@@ -17,9 +17,15 @@
   */
 package net.bluemind.authentication.service.internal;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import net.bluemind.core.api.Email;
 import net.bluemind.core.api.fault.ServerFault;
 import net.bluemind.core.container.model.ItemValue;
 import net.bluemind.core.rest.BmContext;
+import net.bluemind.domain.api.Domain;
+import net.bluemind.domain.api.IDomains;
 import net.bluemind.user.api.User;
 import net.bluemind.user.hook.DefaultUserHook;
 
@@ -27,18 +33,35 @@ public class AuthContextUserHook extends DefaultUserHook {
 
 	@Override
 	public void onUserCreated(BmContext context, String domainUid, ItemValue<User> fresh) throws ServerFault {
-		AuthContextCache.getInstance().getCache().invalidate(fresh.value.login + "@" + domainUid);
+		AuthContextCache.getInstance().getCache().invalidateAll(cacheKeys(context, domainUid, fresh));
 	}
 
 	@Override
 	public void onUserUpdated(BmContext context, String domainUid, ItemValue<User> previous, ItemValue<User> current)
 			throws ServerFault {
-		AuthContextCache.getInstance().getCache().invalidate(previous.value.login + "@" + domainUid);
+		Set<String> keys = cacheKeys(context, domainUid, previous);
+		keys.addAll(cacheKeys(context, domainUid, current));
+		AuthContextCache.getInstance().getCache().invalidateAll(keys);
 	}
 
 	@Override
 	public void onUserDeleted(BmContext context, String domainUid, ItemValue<User> deleted) throws ServerFault {
-		AuthContextCache.getInstance().getCache().invalidate(deleted.value.login + "@" + domainUid);
+		AuthContextCache.getInstance().getCache().invalidateAll(cacheKeys(context, domainUid, deleted));
+	}
+
+	private Set<String> cacheKeys(BmContext context, String domainUid, ItemValue<User> u) {
+		ItemValue<Domain> domAliases = context.provider().instance(IDomains.class).get(domainUid);
+		Set<String> expanded = new HashSet<>();
+		expanded.add(u.value.login + "@" + domainUid);
+		for (Email e : u.value.emails) {
+			if (e.allAliases) {
+				expanded.add(e.localPart() + "@" + domAliases.uid);
+				domAliases.value.aliases.forEach(al -> expanded.add(e.localPart() + "@" + al));
+			} else {
+				expanded.add(e.address);
+			}
+		}
+		return expanded;
 	}
 
 }
