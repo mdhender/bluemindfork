61f14bc9da3083873c38c6ac336c0911f1fda105
blandine <blandine.descamps@gmail.com>
2019-11-15 01:58:34 -0800

    FEATWEBML-397 : change alert messages keys

--- ui/mail-webapp/net.bluemind.webapp.mail.alert.js/index.js
+++ ui/mail-webapp/net.bluemind.webapp.mail.alert.js/index.js
@@ -3,58 +3,58 @@ import { AlertTypes } from "@bluemind/alert.store";
 export default {
     "MSG_MOVED_LOADING": {
         type: AlertTypes.LOADING,
-        key: "mail.alert.move.loading"
+        key: "alert.mail.move.loading"
     },
     "MSG_MOVE_OK": {
         type: AlertTypes.SUCCESS,
-        key: "mail.alert.move.ok"
+        key: "alert.mail.move.ok"
     },
     "MSG_MOVE_ERROR": {
         type: AlertTypes.ERROR,
-        key: "mail.alert.move.error"
+        key: "alert.mail.move.error"
     },
     "MSG_REMOVED_LOADING": {
         type: AlertTypes.LOADING,
-        key : "common.alert.remove.loading"
+        key : "alert.common.remove.loading"
     },
     "MSG_REMOVED_OK": {
         type: AlertTypes.SUCCESS,
-        key: "common.alert.remove.ok"
+        key: "alert.common.remove.ok"
     },
     "MSG_REMOVED_ERROR": {
         type: AlertTypes.ERROR,
-        key: "common.alert.remove.error"
+        key: "alert.common.remove.error"
     },
     "MSG_SEND_LOADING": {
         type: AlertTypes.LOADING,
-        key: "mail.alert.send.loading"
+        key: "alert.mail.send.loading"
     },
     "MSG_SENT_OK": {
         type: AlertTypes.SUCCESS,
-        key: "mail.alert.message.sent.ok"
+        key: "alert.mail.message.sent.ok"
     },
     "MSG_SENT_ERROR": {
         type: AlertTypes.ERROR,
-        key: "mail.alert.message.sent.error"
+        key: "alert.mail.message.sent.error"
     },
     "MSG_PURGE_LOADING": {
         type: AlertTypes.LOADING,
-        key: "common.alert.purge.loading"
+        key: "alert.common.purge.loading"
     },
     "MSG_PURGE_OK": {
         type: AlertTypes.SUCCESS,
-        key: "common.alert.purge.ok"
+        key: "alert.common.purge.ok"
     },
     "MSG_PURGE_ERROR": {
         type: AlertTypes.ERROR,
-        key: "common.alert.purge.error"
+        key: "alert.common.purge.error"
     },
     "MSG_DRAFT_DELETE_OK": {
         type: AlertTypes.SUCCESS,
-        key: "mail.alert.message.draft.delete.ok"
+        key: "alert.mail.message.draft.delete.ok"
     },
     "MSG_DRAFT_DELETE_ERROR": {
         type: AlertTypes.ERROR,
-        key: "mail.alert.message.draft.delete.error"
+        key: "alert.mail.message.draft.delete.error"
     }
 };
