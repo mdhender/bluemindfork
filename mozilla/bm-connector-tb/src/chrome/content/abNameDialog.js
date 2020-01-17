b5c405bc2b9114099314d24c20db469c242e20c4
Nicolas Lascombes <nicolas.lascombes@blue-mind.net>
2020-01-17 11:41:03 +0100

    BM-15258 Fix: tbird, bm addressbooks can be managed from settings tab now

--- mozilla/bm-connector-tb/src/chrome/content/abNameDialog.js
+++ mozilla/bm-connector-tb/src/chrome/content/abNameDialog.js
@@ -25,50 +25,10 @@ Components.utils.import("resource://bm/bmService.jsm");
 
 var gBMAbNameDlgLogger = Components.classes["@blue-mind.net/logger;1"].getService().wrappedJSObject.getLogger("gBMabNameDlg: ");
 
-function abNameOKButton() {
-    let isLocal = document.getElementById("local").getAttribute("checked") == "false";
-    var newName = gNameInput.value.trim();
-    if (!isLocal) {
-        newName = bmUtils.getBmAddressbookName(newName, false);
-    }
-    if (gDirectory) {
-        gDirectory.dirName = newName;
-        gBMAbNameDlgLogger.debug("directory [" + newName + "] updated");
-        if (!isLocal) {
-            if (!bmUtils.getBoolPref(gDirectory.URI + ".bm-added", false)) {
-                bmUtils.setBoolPref(gDirectory.URI + ".bm-updated", true);
-                gBMAbNameDlgLogger.debug("=> directory marked as updated");
-            }
-        }
-    } else {
-        let pref = Components.classes["@mozilla.org/abmanager;1"]
-            .getService(Components.interfaces.nsIAbManager)
-            .newAddressBook(newName, "", 2);
-        gBMAbNameDlgLogger.debug("directory [" + newName + "] added");
-        if (!isLocal) {
-            //calculate uri
-            let uri = "moz-abmdbdirectory://" + bmUtils.getCharPref(pref + ".filename");
-            //set temp bm-id
-            let tempId = "temp-" + bmUtils.randomUUID();
-            bmUtils.setCharPref(uri + ".bm-id", tempId);
-            bmUtils.setBoolPref(uri + ".bm-added", true);
-            bmUtils.setBoolPref(uri + ".bm-writable", true);
-            bmUtils.setBoolPref(uri + ".bm-manageable", true);
-            bmUtils.setBoolPref(uri + ".bm-isDefault", false);
-            gBMAbNameDlgLogger.debug("=> directory marked as added");
-            bmService.monitor.listenDirectory(uri, tempId);
-        }
-    }
-    
-    return true;
-}
-
 function BMCheckIsLocal() {
     if (gDirectory) {
         let id = bmUtils.getCharPref(gDirectory.URI + ".bm-id", null);
-        let checkbox = document.getElementById("local");
         if (id != null) {
-            checkbox.setAttribute("checked", "true");
             let isDefault = bmUtils.getBoolPref(gDirectory.URI + ".bm-default", false);
             let isManageable = bmUtils.getBoolPref(gDirectory.URI + ".bm-manageable", false);
             if (isDefault || !isManageable) {
@@ -79,10 +39,7 @@ function BMCheckIsLocal() {
             let ownedBy = bmUtils.getLocalizedString("abname.ownedby") + ": "
                             +  bmUtils.getCharPref(gDirectory.URI + ".bm-ownerDisplayName", "");
             document.getElementById("sharedBy").setAttribute("value", ownedBy);
-        } else {
-            checkbox.setAttribute("checked", "false");
         }
-        checkbox.setAttribute("disabled", "true");
     }
 }
 
