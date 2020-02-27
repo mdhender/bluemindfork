fc569a778833b1b18414145d5cf524dadaa9a998
Laurent Coustet <laurent.coustet@bluemind.net>
2020-02-27 17:58:26 +0100

    Bugfix: BM-15731: fix postgresql dependencies.
    When we modify PostgreSQL, we must be sure all PG_ dependencies
    matches to the latest version from PGDG.
    
    This commits adds two tools. One to retrieve PGDG latest major version
    from the debian repositories.
    The other one is much more complex for RHEL (parses XML, remove xml namespace, ...)
    
    The output of both scripts matches what EXTDEPS requires.
    
    Example:
    ```
    ed@soloviei $> ./EXTDEPS-getlatestpg.sh
    PG_JESSIE="11.7-2.pgdg80+1"
    PGCOMMON_JESSIE="213.pgdg80+1"
    PG_STRETCH="11.7-2.pgdg90+1"
    PGCOMMON_STRETCH="213.pgdg90+1"
    PG_BUSTER="11.7-2.pgdg100+1"
    PGCOMMON_BUSTER="213.pgdg100+1"
    PG_XENIAL="11.7-2.pgdg16.04+1"
    PGCOMMON_XENIAL="213.pgdg16.04+1"
    PG_BIONIC="11.7-2.pgdg18.04+1"
    PGCOMMON_BIONIC="213.pgdg18.04+1"
    PG_RHEL7="11.7-1PGDG.rhel7"
    ```

---
+++ packaging/EXTDEPS-getlatestpg-rpm.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import gzip
+import posixpath
+import re
+try:
+    import requests
+except ImportError:
+    print("sudo apt install python3-requests ? ;)")
+    raise
+import sys
+import xml.etree.ElementTree as ET
+from urllib.parse import urljoin
+
+
+def get(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("URL {} error {}: {}".format(url, r.status_code, r.text))
+        assert r.status_code != 200
+    return r
+
+def main(repomd_baseurl, postgresql_version, rhel_version):
+    r = get(urljoin(repomd_baseurl, "repodata/repomd.xml"))
+    xmldata = re.sub(' xmlns="[^"]+"', '', r.text, count=1)
+    root = ET.fromstring(xmldata)
+    primary_location = root.findall("./data[@type='primary']/location")[0].get("href")
+
+    r = get(urljoin(repomd_baseurl, primary_location))
+    primary_xmldata = re.sub(' xmlns="[^"]+"', '', gzip.decompress(r.content).decode("utf-8"), count=1)
+    root = ET.fromstring(primary_xmldata)
+    pkg_versions = [
+        "{}-{}".format(e.get("ver"), e.get("rel"))
+        for e in root.findall("package[@type='rpm']/name/[.='postgresql{}']/../version".format(postgresql_version))
+    ]
+    print('PG_RHEL{}="{}"'.format(rhel_version, sorted(pkg_versions)[-1]))
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("postgresql_version",
+        help="PostgreSQL major version (eg. 11)")
+    parser.add_argument("--rhel-version", default="7", help="RHEL version (eg. 7)")
+    parser.add_argument("--arch", default="x86_64", help="Architecture")
+    parser.add_argument("--yum-repomd-base",
+        default="https://download.postgresql.org/pub/repos/yum/srpms/",
+        help="Yum repository base (see default value)")
+    args = parser.parse_args()
+    repomd_baseurl = urljoin(
+        args.yum_repomd_base,
+        "{postgresql_version}/redhat/rhel-{rhel_version}-{arch}/".format(
+            postgresql_version=args.postgresql_version,
+            rhel_version=args.rhel_version,
+            arch=args.arch)
+    )
+    main(
+        repomd_baseurl=repomd_baseurl,
+        postgresql_version=args.postgresql_version,
+        rhel_version=args.rhel_version)
+    sys.exit(0)
---
+++ packaging/EXTDEPS-getlatestpg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Allow to retrieve latest packaged version of a selected PostgreSQL version.
+
+set -e
+
+APT_DISTNAMES="xenial bionic jessie stretch buster"  # Use space separator
+RPM_RHEL_VERSIONS="7"  # Use space separator
+
+
+PGVERSION=$1
+[ ! -z "${PGVERSION}" ] && shift || PGVERSION=11
+TEMPDIR=$(mktemp -d --suffix .pgdepcheck)
+
+trap "{ rm -fr \"$TEMPDIR\"; }" EXIT
+
+for dist in ${APT_DISTNAMES}; do
+	outfile="${TEMPDIR}/Packages.${dist}"
+	wget -q -O "${outfile}" "http://apt.postgresql.org/pub/repos/apt/dists/${dist}-pgdg/main/binary-amd64/Packages"
+	pg_pkg_version=$(grep "Package: postgresql-${PGVERSION}$" -A5 "${outfile}" | grep 'Version:' | awk '{print $2}')
+	pg_common_pkg_version=$(grep 'Package: postgresql-common$' -A5 "${outfile}" | grep 'Version:' | awk '{print $2}')
+	echo PG_${dist^^}=\"${pg_pkg_version}\"
+	echo PGCOMMON_${dist^^}=\"${pg_common_pkg_version}\"
+done
+# Use the latest distrib to get PGKEYRING, which should be the same for all distributions
+# NOTE: This is an intentional out of the loop variable use
+pg_keyring_version=$(grep "Package: pgdg-keyring" -A5 "${outfile}" | grep 'Version:' | awk '{print $2}')
+echo PGKEYRING=\"${pg_keyring_version}\"
+
+# Redhat
+# https://yum.postgresql.org/srpms/11/redhat/rhel-7-x86_64/
+# https://download.postgresql.org/pub/repos/yum/srpms/11/redhat/rhel-7-x86_64/repodata/repomd.xml
+
+for rhel_version in ${RPM_RHEL_VERSIONS}; do
+	python3 "$(dirname $0)/EXTDEPS-getlatestpg-rpm.py" "$PGVERSION" --rhel-version "${rhel_version}" --arch x86_64
+done
