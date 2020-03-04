cab316b81f1090a80e9c94bdc3d624cf232707cc
Vincent Vignaud <vincent.vignaud@bluemind.net>
2020-03-04 18:30:56 +0100

    Chore: remove console.log

--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/removeAttachment.js
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/removeAttachment.js
@@ -2,15 +2,11 @@ import injector from "@bluemind/inject";
 
 export function removeAttachment({ commit, dispatch, getters, state }, attachmentUid) {
     const status = getters["draft/getAttachmentStatus"](attachmentUid);
-    console.log(status);
-    console.log(getters["draft/getAttachmentStatus"]);
     const partAddress = state.draft.parts.attachments.find(attachment => attachment.uid === attachmentUid).address;
 
     let promise = Promise.resolve();
 
     if (status !== "ERROR") {
-        console.log("coucou");
-        console.log(status);
         const draftbox = getters.my.DRAFTS;
         promise = injector
             .getProvider("MailboxItemsPersistence")
