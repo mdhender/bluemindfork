66961f8e6a072591fb22cc92724c725ce25e9daa
Manuel Guilley <manuel.guilley@bluemind.net>
2020-03-25 14:31:09 +0100

    FEATBL-1036: bm-cli group

---
+++ parent/cli/net.bluemind.cli.group/src/net/bluemind/cli/group/GroupDeleteCommand.java
@@ -0,0 +1,110 @@
+/* BEGIN LICENSE
+  * Copyright © Blue Mind SAS, 2012-2018
+  *
+  * This file is part of BlueMind. BlueMind is a messaging and collaborative
+  * solution.
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of either the GNU Affero General Public License as
+  * published by the Free Software Foundation (version 3 of the License).
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  *
+  * See LICENSE.txt
+  * END LICENSE
+  */
+package net.bluemind.cli.group;
+
+import java.util.Optional;
+
+import io.airlift.airline.Arguments;
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+import net.bluemind.cli.cmd.api.CliContext;
+import net.bluemind.cli.cmd.api.ICmdLet;
+import net.bluemind.cli.cmd.api.ICmdLetRegistration;
+import net.bluemind.cli.utils.CliUtils;
+import net.bluemind.cli.utils.Tasks;
+import net.bluemind.core.container.model.ItemValue;
+import net.bluemind.core.task.api.TaskRef;
+import net.bluemind.core.task.api.TaskStatus;
+import net.bluemind.group.api.Group;
+import net.bluemind.group.api.IGroup;
+
+@Command(name = "delete", description = "delete group")
+public class GroupDeleteCommand implements ICmdLet, Runnable  {
+
+	public static class Reg implements ICmdLetRegistration {
+
+		@Override
+		public Optional<String> group() {
+			return Optional.of("group");
+		}
+
+		@Override
+		public Class<? extends ICmdLet> commandClass() {
+			return GroupDeleteCommand.class;
+		}
+	}
+
+	@Arguments(required = true, description = "groupName@domain or domain name")
+	public String target;
+	
+	@Option(name = "--dry", description = "Dry-run (do nothing)")
+	public boolean dry;
+
+	protected CliContext ctx;
+	protected CliUtils cliUtils;
+	
+	
+	@Override
+	public void run() {	
+		if(target.contains("@")) {
+			deleteGroupbyName();
+		} else {
+			deleteAllGroups();
+		}
+	}
+	
+	private void deleteGroupbyName() {
+		String domainUid = target.split("@")[1];
+		String name = target.split("@")[0];
+		
+		IGroup groupApi = ctx.adminApi().instance(IGroup.class, domainUid);
+		ItemValue<Group> group = groupApi.byName(name);
+		if (group != null){
+			deleteGroup(groupApi, group.uid);
+		}
+			
+	}
+	
+	private void deleteAllGroups() {
+		IGroup groupApi = ctx.adminApi().instance(IGroup.class, target);
+		groupApi.allUids().forEach(uid -> deleteGroup(groupApi, uid));
+	}
+	
+	private void deleteGroup(IGroup groupApi, String uid) {
+		if (dry) {
+			ctx.info("DRY : delete " + uid);
+		} else {
+			TaskRef tr = groupApi.delete(uid);
+			TaskStatus status = Tasks.follow(ctx, tr, String.format("Failed to delete entry %s", uid));
+
+			if (status == null || status.state != TaskStatus.State.Success) {
+				ctx.error("Failed to delete group " + uid);
+			} else {
+				ctx.info("Group " + uid+ " deleted");
+			}
+		}
+	}
+
+	@Override
+	public Runnable forContext(CliContext ctx) {
+		this.ctx = ctx;
+		this.cliUtils = new CliUtils(ctx);
+		return this;
+	}
+
+}
---
+++ parent/cli/net.bluemind.cli.group/src/net/bluemind/cli/group/GroupUpdateCommand.java
@@ -0,0 +1,153 @@
+/* BEGIN LICENSE
+  * Copyright © Blue Mind SAS, 2012-2018
+  *
+  * This file is part of BlueMind. BlueMind is a messaging and collaborative
+  * solution.
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of either the GNU Affero General Public License as
+  * published by the Free Software Foundation (version 3 of the License).
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  *
+  * See LICENSE.txt
+  * END LICENSE
+  */
+package net.bluemind.cli.group;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import io.airlift.airline.Arguments;
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+import net.bluemind.cli.cmd.api.CliContext;
+import net.bluemind.cli.cmd.api.ICmdLet;
+import net.bluemind.cli.cmd.api.ICmdLetRegistration;
+import net.bluemind.cli.utils.CliUtils;
+import net.bluemind.core.api.ListResult;
+import net.bluemind.core.container.model.ItemValue;
+import net.bluemind.directory.api.BaseDirEntry.Kind;
+import net.bluemind.directory.api.DirEntry;
+import net.bluemind.directory.api.DirEntryQuery;
+import net.bluemind.directory.api.IDirectory;
+import net.bluemind.group.api.Group;
+import net.bluemind.group.api.IGroup;
+import net.bluemind.group.api.Member;
+import net.bluemind.group.api.Member.Type;
+
+@Command(name = "update", description = "update users")
+public class GroupUpdateCommand implements ICmdLet, Runnable   {
+
+	public static class Reg implements ICmdLetRegistration {
+
+		@Override
+		public Optional<String> group() {
+			return Optional.of("group");
+		}
+
+		@Override
+		public Class<? extends ICmdLet> commandClass() {
+			return GroupUpdateCommand.class;
+		}
+	}
+	
+	@Arguments(required = true, description = "groupName@domain")
+	public String target;
+	
+	@Option(name = "--add-members", description = "Add Members, use uid separated by spaces")
+	public String appendMembers;
+	
+	@Option(name = "--remove-members", description = "Remove Members, use 'all' to empty group"
+			+ "OR uid separated by spaces.")
+	public String deleteMembers;
+	
+	@Option(name = "--extId", description = "update group externalId")
+	public String extId;
+	
+	@Option(name = "--description", description = "update group description")
+	public String description;
+	
+	protected CliContext ctx;
+	protected CliUtils cliUtils;
+
+	@Override
+	public Runnable forContext(CliContext ctx) {
+		this.ctx = ctx;
+		this.cliUtils = new CliUtils(ctx);
+		return this;
+	}
+	
+	@Override
+	public void run() {
+		if(target.contains("@")) {
+			String domainUid = target.split("@")[1];
+			String name = target.split("@")[0];			
+		
+			IGroup groupApi = ctx.adminApi().instance(IGroup.class, domainUid);
+			ItemValue<Group> group = groupApi.byName(name);
+			
+			if (extId != null) {
+				groupApi.setExtId(group.uid, extId.trim().isEmpty() ? null : extId);
+			}
+			if (description != null) {
+				group.value.description = description;
+				groupApi.update(group.uid, group.value);
+			}
+			if (deleteMembers != null) {
+				removeMembers(groupApi, group, domainUid);
+			}
+			if (appendMembers != null) {
+				addMembers(groupApi, group, domainUid);
+			}
+		} else {
+			ctx.info("Group not found.");
+		}
+	}
+	
+	private void addMembers(IGroup groupApi, ItemValue<Group> group, String domainUid){
+		List<String> uids = Arrays.asList(appendMembers.split(" "));
+		List<Member> allMembers = new ArrayList<>();
+		
+		allMembers.addAll(getMembersList(uids, domainUid));
+		groupApi.add(group.uid, allMembers);
+	}
+	
+	private void removeMembers(IGroup groupApi, ItemValue<Group> group, String domainUid){
+		if (deleteMembers.equalsIgnoreCase("all")) {
+			groupApi.remove(group.uid, groupApi.getMembers(group.uid));
+		} else {
+			List<String> uids = Arrays.asList(deleteMembers.split(" "));			
+			groupApi.remove(group.uid, getMembersList(uids, domainUid));
+		}
+	}
+	
+	
+	private List<Member> getMembersList(List<String> entryUids, String domainUid){
+		List<Member> memberList = new ArrayList<>();
+		IDirectory dirApi = ctx.adminApi().instance(IDirectory.class, domainUid);
+		DirEntryQuery dirQuery = new DirEntryQuery();
+		dirQuery.entryUidFilter = entryUids;
+		ListResult<ItemValue<DirEntry>> entries = dirApi.search(dirQuery);
+		for(ItemValue<DirEntry> entry : entries.values) {
+			Member member = new Member();
+			member.uid = entry.uid;
+			if(entry.value.kind == Kind.USER) {
+				member.type = Type.user;
+			}
+			if(entry.value.kind == Kind.GROUP) {
+				member.type = Type.group;
+			}
+			if(entry.value.kind == Kind.EXTERNALUSER) {
+				member.type = Type.external_user;
+			}	
+			memberList.add(member);
+		}
+		return memberList;
+	}
+
+}
