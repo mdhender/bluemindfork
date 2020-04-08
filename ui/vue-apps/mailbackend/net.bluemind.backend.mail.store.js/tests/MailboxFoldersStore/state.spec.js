5426b0e24c167f0f4a0ee64d10367ad09c03aaac
Enguerran Colson <enguerran.colson@bluemind.net>
2020-04-08 15:51:03 +0200

    FEATWEBML-460 Refactor: Test static objects with less code

--- ui/vue-apps/mailbackend/net.bluemind.backend.mail.store.js/tests/MailboxFoldersStore/state.spec.js
+++ ui/vue-apps/mailbackend/net.bluemind.backend.mail.store.js/tests/MailboxFoldersStore/state.spec.js
@@ -1,16 +1,7 @@
 import { state } from "../../src/MailboxFoldersStore/state";
 
-describe("[MailItemsStore][state] : initial state", () => {
-    test("contains an empty object 'items'", () => {
-        expect(state.items).toEqual({});
-    });
-    test("contains a empty array 'itemKeys'", () => {
-        expect(state.itemKeys).toEqual([]);
-    });
-    test("contains a empty array 'itemByContainers'", () => {
-        expect(state.itemsByContainer).toEqual({});
-    });
-    test("no to contain anything else", () => {
-        expect(Object.keys(state)).toEqual(["items", "itemKeys", "itemsByContainer"]);
+describe("[MailItemsStore][state]", () => {
+    test("initial state", () => {
+        expect(state).toMatchSnapshot();
     });
 });
--- ui/vue-apps/mailbackend/net.bluemind.backend.mail.store.js/tests/MailboxItemsStore/state.spec.js
+++ ui/vue-apps/mailbackend/net.bluemind.backend.mail.store.js/tests/MailboxItemsStore/state.spec.js
@@ -1,19 +1,7 @@
 import { state } from "../../src/MailboxItemsStore/state";
 
-describe("[MailItemsStore][state] : Initial state", () => {
-    test("contains  'items'", () => {
-        expect(state.items).toEqual({});
-    });
-    test("contains a empty array 'partKeys'", () => {
-        expect(state.itemKeys).toEqual([]);
-    });
-    test("contains an empty object 'partContents'", () => {
-        expect(state.partContents).toEqual({});
-    });
-    test("contains a empty object 'itemsParts'", () => {
-        expect(state.itemsParts).toEqual({});
-    });
-    test("not to contain anything else", () => {
-        expect(Object.keys(state)).toEqual(["items", "partContents", "itemKeys", "itemsParts"]);
+describe("[MailItemsStore][state]", () => {
+    test("initial state", () => {
+        expect(state).toMatchSnapshot();
     });
 });
