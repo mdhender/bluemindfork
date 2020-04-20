6ff7ed0aeb91fe1f876f7e5c20d73d55a3dd1215
kladier <kevin.ladier@bluemind.net>
2020-04-20 18:47:48 +0200

    Impr: refactor send action, split it into functions

--- ui/vue-apps/commons/net.bluemind.alert.store.js/src/mutations/addApplicationAlert.js
+++ ui/vue-apps/commons/net.bluemind.alert.store.js/src/mutations/addApplicationAlert.js
@@ -1,6 +1,5 @@
 import AlertFactory from "../AlertFactory";
 
 export function addApplicationAlert(state, { code, props, uid }) {
-    console.log("HELLO addApplicationAlert mutation !!!!");
     state.applicationAlerts.push(AlertFactory.create(code, props, uid));
 }
--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/send.js
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/send.js
@@ -1,5 +1,3 @@
-//FIXME: Refactor this...
-
 import { DraftStatus } from "@bluemind/backend.mail.store";
 import { EmailValidator, Flag } from "@bluemind/email";
 import injector from "@bluemind/inject";
@@ -9,8 +7,112 @@ import ItemUri from "@bluemind/item-uri";
 /** Send the last draft: move it to the Outbox then flush. */
 export function send({ state, commit, getters, dispatch }) {
     const draft = state.draft;
-    let draftId = draft.id,
-        sentbox;
+    let draftId = draft.id;
+    const loadingAlertUid = addLoadingAlert(commit, draft.subject);
+
+    return dispatch("saveDraft")
+        .then(newDraftId => {
+            draftId = newDraftId;
+            commit("draft/update", { status: DraftStatus.SENDING });
+            validateDraft(draft);
+            return moveToOutbox(getters.my, draftId);
+        })
+        .then(moveResult => {
+            if (!moveResult || moveResult.status !== "SUCCESS") {
+                throw "Unable to flush the Outbox.";
+            }
+            draftId = moveResult.doneIds[0].destination;
+            return flush(); // flush means send mail + move to sentbox
+        })
+        .then(taskResult => getSentMessageId(taskResult, draftId, getters.my.SENT.uid))
+        .then(mailItem => handleSuccess(mailItem.internalId, loadingAlertUid, getters.my.SENT, draft, commit, dispatch))
+        .catch(reason => handleError(commit, draft.subject, loadingAlertUid, reason));
+}
+
+function getSentMessageId(taskResult, draftId, sentboxUid) {
+    if (taskResult.result && Array.isArray(taskResult.result)) {
+        let importedMailboxItem = taskResult.result.find(r => r.source === draftId);
+        return injector
+            .getProvider("MailboxItemsPersistence")
+            .get(sentboxUid)
+            .getCompleteById(importedMailboxItem.destination);
+    } else {
+        throw "Unable to retrieve task result";
+    }
+}
+
+function flush() {
+    return injector
+        .getProvider("OutboxPersistence")
+        .get()
+        .flush()
+        .then(taskRef => {
+            // wait for the flush of the outbox to be finished
+            const taskService = injector.getProvider("TaskService").get(taskRef.id);
+            return retrieveTaskResult(taskService, 250, 5);
+        });
+}
+
+function moveToOutbox(my, draftId) {
+    return injector
+        .getProvider("MailboxFoldersPersistence")
+        .get(my.mailboxUid)
+        .importItems(my.OUTBOX.internalId, {
+            mailboxFolderId: my.DRAFTS.internalId,
+            ids: [{ id: draftId }],
+            expectedIds: undefined,
+            deleteFromSource: true
+        });
+}
+
+function handleSuccess(sentMailId, loadingAlertUid, sentbox, draft, commit, dispatch) {
+    manageFlagOnPreviousMessage(draft, dispatch);
+    const messageKey = ItemUri.encode(sentMailId, sentbox.uid);
+    commit("removeApplicationAlert", loadingAlertUid, { root: true });
+    commit(
+        "addApplicationAlert",
+        {
+            code: "MSG_SENT_OK",
+            props: {
+                subject: draft.subject,
+                subjectLink: {
+                    name: "v:mail:message",
+                    params: { message: messageKey, folder: sentbox.value.fullName }
+                }
+            }
+        },
+        { root: true }
+    );
+    commit("draft/update", { status: DraftStatus.SENT, id: null, saveDate: null });
+}
+
+function manageFlagOnPreviousMessage(draft, dispatch) {
+    if (draft.previousMessage && draft.previousMessage.action) {
+        let action = draft.previousMessage.action;
+        let mailboxItemFlag;
+        if (action === "replyAll" || action === "reply") {
+            mailboxItemFlag = Flag.ANSWERED;
+        } else if (action === "forward") {
+            mailboxItemFlag = Flag.FORWARDED;
+        }
+        dispatch("messages/addFlag", { messageKeys: [draft.previousMessage.messageKey], mailboxItemFlag });
+    }
+}
+
+function handleError(commit, draftSubject, loadingAlertUid, reason) {
+    commit("removeApplicationAlert", loadingAlertUid, { root: true });
+    commit(
+        "addApplicationAlert",
+        {
+            code: "MSG_SENT_ERROR",
+            props: { subject: draftSubject, reason: reason.message }
+        },
+        { root: true }
+    );
+    commit("draft/update", { status: DraftStatus.SENT });
+}
+
+function addLoadingAlert(commit, draftSubject) {
     const loadingAlertUid = UUIDGenerator.generate();
 
     commit(
@@ -18,111 +120,23 @@ export function send({ state, commit, getters, dispatch }) {
         {
             code: "MSG_SEND_LOADING",
             uid: loadingAlertUid,
-            props: { subject: draft.subject }
+            props: { subject: draftSubject }
         },
         { root: true }
     );
 
-    // ensure the last draft is up to date
-    return dispatch("saveDraft")
-        .then(newDraftId => {
-            draftId = newDraftId;
-            commit("draft/update", { status: DraftStatus.SENDING });
-
-            // validation
-            if (!validate(draft)) {
-                throw injector
-                    .getProvider("i18n")
-                    .get()
-                    .t("mail.error.email.address.invalid");
-            }
-            return Promise.resolve();
-        })
-        .then(() => {
-            // move draft from draftbox to outbox
-            const draftbox = getters.my.DRAFTS;
-            const outbox = getters.my.OUTBOX;
-            return injector
-                .getProvider("MailboxFoldersPersistence")
-                .get(getters.my.mailboxUid)
-                .importItems(outbox.internalId, {
-                    mailboxFolderId: draftbox.internalId,
-                    ids: [{ id: draftId }],
-                    expectedIds: undefined,
-                    deleteFromSource: true
-                });
-        })
-        .then(moveResult => {
-            // flush the outbox
-            if (!moveResult || moveResult.status !== "SUCCESS") {
-                throw "Unable to flush the Outbox.";
-            }
-            draftId = moveResult.doneIds[0].destination;
-            const outboxService = injector.getProvider("OutboxPersistence").get();
-            return outboxService.flush();
-        })
-        .then(taskRef => {
-            // wait for the flush of the outbox to be finished (flush means send mail + move to sentbox)
-            const taskService = injector.getProvider("TaskService").get(taskRef.id);
-            return retrieveTaskResult(taskService, 250, 5);
-        })
-        .then(taskResult => {
-            // compute and return the IMAP id of the mail inside the sentbox
-            if (taskResult.result && Array.isArray(taskResult.result)) {
-                let importedMailboxItem = taskResult.result.find(r => r.source === draftId);
-                sentbox = getters.my.SENT;
-                const sentboxItemsService = injector.getProvider("MailboxItemsPersistence").get(sentbox.uid);
-                return sentboxItemsService.getCompleteById(importedMailboxItem.destination);
-            } else {
-                throw "Unable to retrieve task result";
-            }
-        })
-        .then(mailItem => {
-            if (draft.previousMessage && draft.previousMessage.action) {
-                let action = draft.previousMessage.action;
-                let mailboxItemFlag;
-                if (action === "replyAll" || action === "reply") {
-                    mailboxItemFlag = Flag.ANSWERED;
-                } else if (action === "forward") {
-                    mailboxItemFlag = Flag.FORWARDED;
-                }
-                dispatch("messages/addFlag", { messageKeys: [draft.previousMessage.messageKey], mailboxItemFlag });
-            }
-            const messageKey = ItemUri.encode(mailItem.internalId, getters.my.SENT.uid);
-            commit("removeApplicationAlert", loadingAlertUid, { root: true });
-            commit(
-                "addApplicationAlert",
-                {
-                    code: "MSG_SENT_OK",
-                    props: {
-                        subject: draft.subject,
-                        subjectLink: {
-                            name: "v:mail:message",
-                            params: { message: messageKey, folder: sentbox.value.fullName }
-                        }
-                    }
-                },
-                { root: true }
-            );
-            commit("draft/update", { status: DraftStatus.SENT, id: null, saveDate: null });
-        })
-        .catch(reason => {
-            commit("removeApplicationAlert", loadingAlertUid, { root: true });
-            commit(
-                "addApplicationAlert",
-                {
-                    code: "MSG_SENT_ERROR",
-                    props: { subject: draft.subject, reason: reason.message }
-                },
-                { root: true }
-            );
-            commit("draft/update", { status: DraftStatus.SENT });
-        });
+    return loadingAlertUid;
 }
 
-function validate(messageToSend) {
-    let recipients = messageToSend.to.concat(messageToSend.cc).concat(messageToSend.bcc);
-    return recipients.some(recipient => EmailValidator.validateAddress(recipient));
+function validateDraft(draft) {
+    let recipients = draft.to.concat(draft.cc).concat(draft.bcc);
+    const allRecipientsAreValid = recipients.some(recipient => EmailValidator.validateAddress(recipient));
+    if (!allRecipientsAreValid) {
+        throw injector
+            .getProvider("i18n")
+            .get()
+            .t("mail.error.email.address.invalid");
+    }
 }
 
 /** Wait for the task to be finished or a timeout is reached. */
