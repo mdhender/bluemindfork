6d2c6f87d16a63fa61b55cc1d4cd89f4bb6afd28
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2020-04-30 12:11:18 +0200

    Chore: do not use count(*) when you want 'exists'

--- parent/mailbox/net.bluemind.mailbox.persistence/src/net/bluemind/mailbox/persistence/MailboxStore.java
+++ parent/mailbox/net.bluemind.mailbox.persistence/src/net/bluemind/mailbox/persistence/MailboxStore.java
@@ -32,6 +32,8 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Splitter;
+
 import net.bluemind.core.api.Email;
 import net.bluemind.core.container.model.Container;
 import net.bluemind.core.container.model.Item;
@@ -52,13 +54,6 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 		@Override
 		public Mailbox create(ResultSet con) throws SQLException {
 			return new Mailbox();
-		}
-	};
-
-	private static final Creator<Integer> INTEGER_CREATOR = new Creator<Integer>() {
-		@Override
-		public Integer create(ResultSet con) throws SQLException {
-			return con.getInt(1);
 		}
 	};
 
@@ -172,10 +167,8 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ "  )" //
 				+ " AND item.container_id = ?";
 
-		String mailboxUid = unique(query, StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
+		return unique(query, StringCreator.FIRST, Collections.emptyList(),
 				new Object[] { email, leftPart, leftPart, container.id });
-
-		return mailboxUid;
 	}
 
 	/**
@@ -191,10 +184,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ " WHERE m.type = ?::enum_mailbox_type" //
 				+ " AND item.container_id = ?";
 
-		List<String> mailboxUids = select(query, StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
-				new Object[] { type.name(), container.id });
-
-		return mailboxUids;
+		return select(query, StringCreator.FIRST, Collections.emptyList(), new Object[] { type.name(), container.id });
 	}
 
 	/**
@@ -210,10 +200,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ " WHERE e.name = ?" //
 				+ " AND item.container_id = ?";
 
-		String mailboxUid = unique(query, StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
-				new Object[] { name, container.id });
-
-		return mailboxUid;
+		return unique(query, StringCreator.FIRST, Collections.emptyList(), new Object[] { name, container.id });
 	}
 
 	public boolean nameAlreadyUsed(Long itemId, Mailbox mailbox) throws SQLException {
@@ -221,7 +208,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 			return false;
 		}
 
-		String query = "SELECT count(*) FROM t_mailbox ";
+		String query = "SELECT 1 FROM t_mailbox ";
 		query += "INNER JOIN t_container_item ON t_container_item.id = t_mailbox.item_id ";
 		query += "WHERE container_id = ? AND name = ?";
 
@@ -230,15 +217,12 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 			query += " AND item_id != ?";
 			parameters = new Object[] { container.id, mailbox.name, itemId };
 		}
+		query = "SELECT EXISTS (" + query + ")";
 
-		Integer total = unique(query, INTEGER_CREATOR, new ArrayList<EntityPopulator<Integer>>(0), parameters);
-
-		if (total != 0) {
-			return true;
-		}
-
-		return false;
+		return unique(query, BooleanCreator.FIRST, Collections.emptyList(), parameters);
 	}
+
+	private static final Splitter AT_SPLITTER = Splitter.on('@');
 
 	public boolean emailAlreadyUsed(Long itemId, Collection<Email> emails) throws SQLException {
 
@@ -246,13 +230,13 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 			return false;
 		}
 
-		ArrayList<String> address = new ArrayList<String>(emails.size());
+		ArrayList<String> address = new ArrayList<>(emails.size());
 		Iterator<Email> it = emails.iterator();
 		while (it.hasNext()) {
 			address.add(it.next().address);
 		}
 
-		String query = "" + "SELECT count(*) FROM t_mailbox_email INNER JOIN t_container_item "
+		String query = "SELECT 1 FROM t_mailbox_email INNER JOIN t_container_item "
 				+ " ON t_container_item.id = t_mailbox_email.item_id " //
 				+ " , ( SELECT ?::text[] as left, ?::text[] as full, ?::text[] as leftall ) as param " + //
 				" WHERE container_id = ? AND ( " //
@@ -260,19 +244,12 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ "    ( all_aliases = false and ( left_address || '@' || right_address = ANY (param.full) OR left_address = ANY(leftall) )) "
 				+ ")";
 
-		String[] left = emails.stream().map(e -> {
-			return e.address.split("@")[0];
-		}).toArray(String[]::new);
+		String[] left = emails.stream().map(e -> AT_SPLITTER.split(e.address).iterator().next()).toArray(String[]::new);
 
-		String[] leftAll = emails.stream().filter(e -> {
-			return e.allAliases;
-		}).map(e -> {
-			return e.address.split("@")[0];
-		}).toArray(String[]::new);
+		String[] leftAll = emails.stream().filter(e -> e.allAliases)
+				.map(e -> AT_SPLITTER.split(e.address).iterator().next()).toArray(String[]::new);
 
-		String[] all = emails.stream().map(e -> {
-			return e.address;
-		}).toArray(String[]::new);
+		String[] all = emails.stream().map(e -> e.address).toArray(String[]::new);
 
 		Object[] parameters = null;
 
@@ -282,15 +259,9 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 		} else {
 			parameters = new Object[] { left, all, leftAll, container.id };
 		}
+		query = "SELECT EXISTS (" + query + ")";
 
-		Integer total = unique(query.toString(), INTEGER_CREATOR, new ArrayList<EntityPopulator<Integer>>(0),
-				parameters);
-
-		if (total != 0) {
-			return true;
-		}
-
-		return false;
+		return unique(query, BooleanCreator.FIRST, Collections.emptyList(), parameters);
 
 	}
 
@@ -302,10 +273,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 		String query = "SELECT item.uid FROM t_mailbox "
 				+ " INNER JOIN t_container_item item ON item_id = item.id WHERE container_id = ? AND quota > 0";
 
-		List<String> uids = select(query, StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
-				new Object[] { container.id });
-
-		return uids;
+		return select(query, StringCreator.FIRST, Collections.emptyList(), new Object[] { container.id });
 	}
 
 	public List<String> routingSearch(Routing routing) throws SQLException {
@@ -315,10 +283,8 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ " WHERE m.routing = ?::enum_mailbox_routing" //
 				+ " AND item.container_id = ?";
 
-		List<String> mailboxUids = select(query, StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
+		return select(query, StringCreator.FIRST, Collections.emptyList(),
 				new Object[] { routing.name(), container.id });
-
-		return mailboxUids;
 	}
 
 	public void deleteEmailByAlias(String alias) throws SQLException {
@@ -329,8 +295,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 	public List<String> allUids() throws SQLException {
 		final String query = "SELECT i.uid FROM t_container_item i JOIN t_mailbox m ON m.item_id = i.id WHERE i.container_id = ?";
 
-		return select(query.toString(), StringCreator.FIRST, Collections.<EntityPopulator<String>>emptyList(),
-				new Object[] { container.id });
+		return select(query, StringCreator.FIRST, Collections.emptyList(), new Object[] { container.id });
 	}
 
 	public Boolean isQuotaGreater(int quotaMax) throws SQLException {
@@ -339,8 +304,7 @@ public class MailboxStore extends AbstractItemValueStore<Mailbox> {
 				+ " INNER JOIN t_container_item item ON m.item_id = item.id " //
 				+ " WHERE m.quota > ? AND item.container_id = ?)";
 
-		return unique(query.toString(), BooleanCreator.FIRST, new ArrayList<EntityPopulator<Boolean>>(0),
-				new Object[] { quotaMax, container.id });
+		return unique(query, BooleanCreator.FIRST, Collections.emptyList(), new Object[] { quotaMax, container.id });
 	}
 
 }
