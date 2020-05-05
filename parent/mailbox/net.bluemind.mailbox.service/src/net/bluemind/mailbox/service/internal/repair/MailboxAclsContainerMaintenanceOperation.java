9bc41d067d4a2b5de07acd5f5452b38a8bfdd100
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2020-05-05 15:52:19 +0200

    BM-15924 Fix: mailbox-acl containers repair was commented out, re-implement it

--- parent/mailbox/net.bluemind.mailbox.service/src/net/bluemind/mailbox/service/internal/repair/MailboxAclsContainerMaintenanceOperation.java
+++ parent/mailbox/net.bluemind.mailbox.service/src/net/bluemind/mailbox/service/internal/repair/MailboxAclsContainerMaintenanceOperation.java
@@ -18,8 +18,12 @@
 package net.bluemind.mailbox.service.internal.repair;
 
 import net.bluemind.core.api.report.DiagnosticReport;
+import net.bluemind.core.container.api.IContainers;
+import net.bluemind.core.container.model.ContainerDescriptor;
 import net.bluemind.core.rest.BmContext;
 import net.bluemind.core.task.service.IServerTaskMonitor;
+import net.bluemind.mailbox.api.IMailboxAclUids;
+import net.bluemind.mailbox.service.internal.MailboxesService;
 import net.bluemind.mailbox.service.internal.repair.MailboxRepairSupport.MailboxMaintenanceOperation;
 
 public class MailboxAclsContainerMaintenanceOperation extends MailboxMaintenanceOperation {
@@ -41,46 +45,32 @@ public class MailboxAclsContainerMaintenanceOperation extends MailboxMaintenance
 
 	private void checkAndRepair(boolean repair, String domainUid, DiagnosticReport report, IServerTaskMonitor monitor) {
 		monitor.begin(1, String.format("Check mailbox %s acls container exists", mailboxToString(domainUid)));
+		IContainers contApi = context.su().provider().instance(IContainers.class);
+		ContainerDescriptor exiting = contApi.getIfPresent(IMailboxAclUids.uidForMailbox(mailbox.uid));
+		if (exiting != null) {
+			monitor.progress(1, String.format("Mailbox %s acls container exists", mailboxToString(domainUid)));
+			report.ok(MAINTENANCE_OPERATION_ID,
+					String.format("Mailbox %s acls container exists", mailboxToString(domainUid)));
+		} else {
+			if (!repair) {
+				monitor.progress(1,
+						String.format("Mailbox %s acls container does not exists", mailboxToString(domainUid)));
+				monitor.end(false, null, null);
 
-		// try {
-		// context.su().provider().instance(IInternalMailboxFolderHierarchy.class,
-		// mailbox.uid);
-		//
-		// monitor.progress(1, String.format("Mailbox %s acls container exists",
-		// mailboxToString(domainUid)));
-		// report.ok(MAINTENANCE_OPERATION_ID,
-		// String.format("Mailbox %s acls container exists",
-		// mailboxToString(domainUid)));
-		// } catch (ServerFault e) {
-		// if (e.getCode() == ErrorCode.NOT_FOUND) {
-		// if (!repair) {
-		// monitor.progress(1,
-		// String.format("Mailbox %s acls container does not exists",
-		// mailboxToString(domainUid)));
-		// monitor.end(false, null, null);
-		//
-		// report.ko(MAINTENANCE_OPERATION_ID,
-		// String.format("Mailbox %s acls container does not exists",
-		// mailboxToString(domainUid)));
-		// return;
-		// }
-		//
-		// monitor.log(
-		// String.format("Mailbox %s acls container not found, creating it",
-		// mailboxToString(domainUid)));
-		//
-		// MailboxesService.Helper.createMailboxesAclsContainer(context,
-		// domainUid, mailbox.uid, mailbox.value);
-		//
-		// monitor.progress(1,
-		// String.format("Mailbox %s acls container repair finished",
-		// mailboxToString(domainUid)));
-		// report.ok(MAINTENANCE_OPERATION_ID,
-		// String.format("Mailbox %s acls container repair finished",
-		// mailboxToString(domainUid)));
-		// }
-		// }
+				report.ko(MAINTENANCE_OPERATION_ID,
+						String.format("Mailbox %s acls container does not exists", mailboxToString(domainUid)));
+			} else {
+				monitor.log(
+						String.format("Mailbox %s acls container not found, creating it", mailboxToString(domainUid)));
 
+				MailboxesService.Helper.createMailboxesAclsContainer(context, domainUid, mailbox.uid, mailbox.value);
+
+				monitor.progress(1,
+						String.format("Mailbox %s acls container repair finished", mailboxToString(domainUid)));
+				report.ok(MAINTENANCE_OPERATION_ID,
+						String.format("Mailbox %s acls container repair finished", mailboxToString(domainUid)));
+			}
+		}
 		monitor.end(true, null, null);
 	}
 }
