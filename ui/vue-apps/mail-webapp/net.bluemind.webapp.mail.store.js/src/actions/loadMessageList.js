e0e98fa05a7b3cf6cf099f232d802bfadeb6287e
Vincent Vignaud <vincent.vignaud@bluemind.net>
2020-05-07 16:49:52 +0200

    FEATWEBML-675 Fix: link to a folder now expands parent folders

--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/loadMessageList.js
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.js/src/actions/loadMessageList.js
@@ -1,9 +1,10 @@
 import { STATUS } from "../constants";
+import ItemUri from "@bluemind/item-uri";
 
 export async function loadMessageList({ dispatch, commit, state, getters }, { folder, mailshare, filter, search }) {
     commit("setStatus", STATUS.LOADING);
 
-    const { key, uid } = locateFolder(folder, mailshare, getters, state);
+    const locatedFolder = locateFolder(folder, mailshare, getters, state);
 
     commit("setMessageFilter", filter);
     commit("messages/clearItems");
@@ -13,12 +14,13 @@ export async function loadMessageList({ dispatch, commit, state, getters }, { fo
     commit("search/setPattern", search);
     commit("deleteAllSelectedMessages");
 
-    await dispatch("selectFolder", key);
+    await dispatch("selectFolder", locatedFolder.key);
+    expandParents(commit, getters, locatedFolder);
 
     if (search) {
         await dispatch("search/search", { pattern: search, filter });
     } else {
-        await dispatch("messages/list", { sorted: state.sorted, folderUid: uid, filter });
+        await dispatch("messages/list", { sorted: state.sorted, folderUid: locatedFolder.uid, filter });
         const sorted = state.messages.itemKeys;
         await dispatch("messages/multipleByKey", sorted.slice(0, 100));
     }
@@ -48,4 +50,14 @@ function getMailshareUid(getters, path) {
         .shift();
     const mailbox = getters.mailshares.filter(ms => ms.root === root).shift();
     return mailbox && mailbox.mailboxUid;
+}
+
+function expandParents(commit, getters, folder) {
+    if (folder.value.parentUid) {
+        commit("expandFolder", folder.value.parentUid);
+        const mailboxId = ItemUri.container(folder.key);
+        const parentFolderKey = ItemUri.encode(folder.value.parentUid, mailboxId);
+        const parentFolder = getters["folders/getFolderByKey"](parentFolderKey);
+        expandParents(commit, getters, parentFolder);
+    }
 }
