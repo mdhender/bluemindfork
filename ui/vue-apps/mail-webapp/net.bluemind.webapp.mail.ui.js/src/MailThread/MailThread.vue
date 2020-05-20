268b88b67a06ab9cee022bd1c5864bb399150259
Vincent Vignaud <vincent.vignaud@bluemind.net>
2020-05-20 16:47:50 +0200

    FEATWEBML-694 Feat: limited actions on read only messages - add alert for read-only message

--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailThread/MailThread.vue
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailThread/MailThread.vue
@@ -2,11 +2,19 @@
     <div class="mail-thread bg-surface">
         <mail-thread-alert
             v-if="message && !areRemoteImagesUnblocked(message.key) && showBlockedImagesAlert"
+            icon="exclamation-circle"
             @close="setShowBlockedImagesAlert(false)"
         >
             {{ $t("mail.content.alert.images.blocked") }}
             &nbsp;
             <a href="#" @click.prevent="showImages()">{{ $t("mail.content.alert.images.show") }}</a>
+        </mail-thread-alert>
+        <mail-thread-alert
+            v-if="isReadOnlyFolder(folderUidOfCurrentMessage) && !isReadOnlyAlertDismissed"
+            icon="info-circle-plain"
+            @close="isReadOnlyAlertDismissed = true"
+        >
+            {{ $t("mail.content.alert.readonly") }}
         </mail-thread-alert>
         <mail-message-new
             v-if="showComposer"
@@ -23,8 +31,9 @@
 
 <script>
 import { mapGetters, mapMutations, mapState } from "vuex";
-import { MimeType } from "@bluemind/email";
 import { computeSubject, previousMessageContent } from "../MessageBuilder";
+import { MimeType } from "@bluemind/email";
+import { ItemUri } from "@bluemind/item-uri";
 import MailMessageContent from "../MailMessageContent";
 import MailMessageNew from "../MailMessageNew";
 import MailMessageNewModes from "../MailMessageNew/MailMessageNewModes";
@@ -39,14 +48,17 @@ export default {
     },
     data() {
         return {
-            userPrefTextOnly: false // TODO: initialize this with user setting,
+            userPrefTextOnly: false, // TODO: initialize this with user setting
+            isReadOnlyAlertDismissed: false
         };
     },
     computed: {
         ...mapGetters("mail-webapp/currentMessage", { message: "message", inlineParts: "content" }),
         ...mapState("mail-webapp", ["showBlockedImagesAlert"]),
-        ...mapGetters("mail-webapp", ["areRemoteImagesUnblocked"]),
-
+        ...mapGetters("mail-webapp", ["areRemoteImagesUnblocked", "currentFolder", "isReadOnlyFolder"]),
+        folderUidOfCurrentMessage() {
+            return ItemUri.container(this.message.key);
+        },
         previousMessage() {
             return {
                 content: previousMessageContent(
@@ -86,6 +98,11 @@ export default {
                     action === this.message.actions.REPLYALL ||
                     action === this.message.actions.FORWARD)
             );
+        }
+    },
+    watch: {
+        message() {
+            this.isReadOnlyAlertDismissed = false;
         }
     },
     methods: {
--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailThread/MailThreadAlert.vue
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailThread/MailThreadAlert.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mail-thread-alert d-flex flex-fill bg-surface">
         <bm-alert show variant="warning" class="d-flex flex-fill justify-content-between text-dark px-3 mb-1">
-            <bm-icon icon="exclamation-circle" class="mr-3 d-block fa-lg text-warning align-self-center" />
+            <bm-icon :icon="icon" class="mr-3 d-block fa-lg text-warning align-self-center" />
             <slot />
             <bm-button-close class="ml-auto" @click="$emit('close')" />
         </bm-alert>
@@ -17,6 +17,12 @@ export default {
         BmAlert,
         BmButtonClose,
         BmIcon
+    },
+    props: {
+        icon: {
+            type: String,
+            required: true
+        }
     }
 };
 </script>
