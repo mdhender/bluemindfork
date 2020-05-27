89683c6927c5faa524af7bc405d6482d4ce20376
kladier <kevin.ladier@bluemind.net>
2020-05-27 19:18:19 +0200

    FEATWEBML-866 Fix: set a tooltip and ellipsis for folder name in empty folder UI

--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailMessageList/FolderResultContentEmptyFolder.vue
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.ui.js/src/MailMessageList/FolderResultContentEmptyFolder.vue
@@ -1,19 +1,23 @@
 <template>
-    <mail-message-list-empty :image="emptyFolderIllustration">
-        <h3 class="d-inline">
-            {{ $t("mail.folder") }}
-            <mail-folder-icon
-                v-if="currentFolder"
-                :shared="currentMailbox.type == 'mailshare'"
-                :folder="currentFolder.value"
-                class="font-weight-bold"
-            />
-        </h3>
-        <h3 class="text-center">{{ $t("mail.empty") }}</h3>
+    <mail-message-list-empty
+        v-bm-tooltip.ds500.viewport
+        :image="emptyFolderIllustration"
+        class="content-empty-folder"
+        :title="currentFolder.value.name"
+    >
+        {{ $t("mail.folder") }}
+        <mail-folder-icon
+            v-if="currentFolder"
+            :shared="currentMailbox.type == 'mailshare'"
+            :folder="currentFolder.value"
+            class="font-weight-bold"
+        />
+        {{ $t("mail.empty") }}
     </mail-message-list-empty>
 </template>
 
 <script>
+import { BmTooltip } from "@bluemind/styleguide";
 import { mapGetters } from "vuex";
 import emptyFolderIllustration from "../../assets/empty-folder.png";
 import MailFolderIcon from "../MailFolderIcon";
@@ -25,6 +29,7 @@ export default {
         MailFolderIcon,
         MailMessageListEmpty
     },
+    directives: { BmTooltip },
     data() {
         return {
             emptyFolderIllustration
@@ -35,3 +40,9 @@ export default {
     }
 };
 </script>
+
+<style>
+.content-empty-folder {
+    word-break: break-all;
+}
+</style>
