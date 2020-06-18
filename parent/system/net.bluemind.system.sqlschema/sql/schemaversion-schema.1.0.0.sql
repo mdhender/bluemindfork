758d45679ff0c418d1d1f62e5e36f7ce7bf7c456
Thomas Fricker <thomas.fricker@blue-mind.net>
2020-06-18 11:40:05 +0200

    Fix: dont create upgrader table

--- parent/system/net.bluemind.system.sqlschema/sql/schemaversion-schema.1.0.0.sql
+++ parent/system/net.bluemind.system.sqlschema/sql/schemaversion-schema.1.0.0.sql
@@ -1,18 +1,6 @@
 create type enum_upgrader_phase as enum 
   ('SCHEMA_UPGRADE', 'POST_SCHEMA_UPGRADE');
   
-create type enum_database_name as enum 
-  ('DIRECTORY', 'SHARD', 'ALL');
-
-create table t_bm_upgraders (
-	server text,
-	phase enum_upgrader_phase,
-	database_name enum_database_name,
-	upgrader_id text,
-	success boolean,
-	PRIMARY KEY (server, database_name, upgrader_id)
-);
-
 create table t_component_version (
     component TEXT primary key,
     version   TEXT not null
