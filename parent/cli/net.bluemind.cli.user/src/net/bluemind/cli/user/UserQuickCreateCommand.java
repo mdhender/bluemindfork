c94cee09d4cac76e430cfa3b78f889608722ac80
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2020-06-22 17:34:48 +0200

    Chore: add a basic 'user quickcreate login@domain' to bm-cli

---
+++ parent/cli/net.bluemind.cli.user/src/net/bluemind/cli/user/UserQuickCreateCommand.java
@@ -0,0 +1,118 @@
+/* BEGIN LICENSE
+  * Copyright © Blue Mind SAS, 2012-2018
+  *
+  * This file is part of BlueMind. BlueMind is a messaging and collaborative
+  * solution.
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of either the GNU Affero General Public License as
+  * published by the Free Software Foundation (version 3 of the License).
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  *
+  * See LICENSE.txt
+  * END LICENSE
+  */
+package net.bluemind.cli.user;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+import io.airlift.airline.Arguments;
+import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+import net.bluemind.addressbook.api.VCard;
+import net.bluemind.addressbook.api.VCard.Identification.FormatedName;
+import net.bluemind.addressbook.api.VCard.Identification.Name;
+import net.bluemind.cli.cmd.api.CliContext;
+import net.bluemind.cli.cmd.api.ICmdLet;
+import net.bluemind.cli.cmd.api.ICmdLetRegistration;
+import net.bluemind.cli.utils.CliUtils;
+import net.bluemind.core.api.Email;
+import net.bluemind.core.email.EmailHelper;
+import net.bluemind.directory.api.BaseDirEntry.AccountType;
+import net.bluemind.hornetq.client.MQ;
+import net.bluemind.mailbox.api.Mailbox.Routing;
+import net.bluemind.server.api.IServer;
+import net.bluemind.user.api.IUser;
+import net.bluemind.user.api.User;
+
+@Command(name = "quickcreate", description = "Create a mail-enabled user")
+public class UserQuickCreateCommand implements ICmdLet, Runnable {
+
+	public static class Reg implements ICmdLetRegistration {
+
+		@Override
+		public Optional<String> group() {
+			return Optional.of("user");
+		}
+
+		@Override
+		public Class<? extends ICmdLet> commandClass() {
+			return UserQuickCreateCommand.class;
+		}
+	}
+
+	@Arguments(required = true, description = "login@domain of the user to create")
+	public String loginAtDomain;
+
+	@Option(name = "--pass", description = "password to apply, otherwise localpart will be used")
+	public String password;
+
+	private CliContext ctx;
+
+	@Override
+	public Runnable forContext(CliContext ctx) {
+		this.ctx = ctx;
+		return this;
+	}
+
+	public void run() {
+		if (!EmailHelper.isValid(loginAtDomain)) {
+			ctx.error(loginAtDomain + " does not look like a valid email.");
+			return;
+		}
+		String[] splitted = loginAtDomain.split("@");
+		String localPart = splitted[0];
+		String domainPart = splitted[1];
+
+		CliUtils cu = new CliUtils(ctx);
+		String dom = cu.getDomainUidFromDomain(domainPart);
+		if (dom == null) {
+			ctx.error(domainPart + " is not a known domain or alias.");
+		}
+		CountDownLatch cdl = new CountDownLatch(1);
+		MQ.init().thenAccept(v -> {
+			User u = new User();
+			u.login = localPart;
+			VCard card = new VCard();
+			card.identification.name = Name.create(domainPart.toUpperCase(), localPart, null, null, null, null);
+			card.identification.formatedName = FormatedName.create(localPart);
+			u.contactInfos = card;
+
+			u.password = Optional.ofNullable(password).orElse(localPart);
+			u.accountType = AccountType.FULL;
+			u.routing = Routing.internal;
+			u.emails = Arrays.asList(Email.create(loginAtDomain, true, false));
+			u.dataLocation = ctx.adminApi().instance(IServer.class, "default").allComplete().stream()
+					.filter(s -> s.value.tags.contains("mail/imap")).findAny().map(s -> s.uid).orElse(null);
+
+			IUser uApi = ctx.adminApi().instance(IUser.class, dom);
+			String uid = "cli-created-" + UUID.randomUUID().toString().toLowerCase();
+			ctx.info("Creating " + uid + " for " + loginAtDomain);
+			uApi.create(uid, u);
+			cdl.countDown();
+		}).whenComplete((v, ex) -> {
+			if (ex != null) {
+				ctx.error(ex.getMessage());
+			} else {
+				ctx.info("finished for " + loginAtDomain);
+			}
+		}).join();
+	}
+
+}
