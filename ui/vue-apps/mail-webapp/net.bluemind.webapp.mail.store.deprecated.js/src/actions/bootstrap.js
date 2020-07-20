88a977bd28e38521b18b60c05f929c033a5b46d4
kladier <kevin.ladier@bluemind.net>
2020-07-20 17:33:32 +0200

    FEATWEBML-939 Fix: dont dispatch loadMessageList twice when app is init with a folder route

--- ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.deprecated.js/src/actions/bootstrap.js
+++ ui/vue-apps/mail-webapp/net.bluemind.webapp.mail.store.deprecated.js/src/actions/bootstrap.js
@@ -1,14 +1,11 @@
 import { Verb } from "@bluemind/core.container.api";
 
-export async function bootstrap({ dispatch, state, getters, commit }, userUid) {
+export async function bootstrap({ dispatch, getters, commit }, userUid) {
     commit("setUserUid", userUid);
 
     try {
         await dispatch("mailboxes/all", { verb: [Verb.Read, Verb.Write, Verb.All], type: "mailboxacl" });
         await dispatch("folders/all", getters.my.mailboxUid);
-        if (!state.currentFolderKey) {
-            await dispatch("loadMessageList", { folder: getters.my.INBOX.key, filter: state.messageFilter });
-        }
         await getters.my.folders.forEach(folder => dispatch("loadUnreadCount", folder.uid));
         await Promise.all(getters.mailshares.map(mailshare => dispatch("folders/all", mailshare.mailboxUid)));
         await dispatch("loadUserSettings");
