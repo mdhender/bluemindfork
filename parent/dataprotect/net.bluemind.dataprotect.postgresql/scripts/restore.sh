0418567a3ea8a0c097f12fa5494010666ed9059b
Laurent Coustet <laurent.coustet@bluemind.net>
2020-10-07 17:05:23 +0200

    [dataprotect] BM-16421 fix: database restoration failed because of incorrect orders of operations.
    Sometimes, dataprotect does not cleanup correctly restored databases.
    Whe had a pg_terminate_backend + dropdb in order to clean them up, but the order of operations
    was mixed up. We droped the just restored database (bj) when restoring bj-data.
    
    Bonus: also fix the migration issue when restoring the bj database. dataLocation should always
    be forced to be "master". Otherwise, the upgrader will try to apply migrations already applied.

--- parent/dataprotect/net.bluemind.dataprotect.postgresql/scripts/restore.sh
+++ parent/dataprotect/net.bluemind.dataprotect.postgresql/scripts/restore.sh
@@ -4,39 +4,13 @@
 
 ## WARNING: variables are "templates", replaced by BlueMind
 
-echo "** Cleanup old dataprotect databases"
-export PGPASSWORD=bj
-
-dp_dbs=`psql -U bj -h localhost -d bj -c "COPY (SELECT datname FROM pg_database WHERE datistemplate=false and datname like 'dp%') TO STDOUT"`
-
-for i in $dp_dbs; do
-    echo "Disconnect all users from database $i"
-    sql_disconnect="SELECT pg_terminate_backend(pg_stat_activity.pid)
-        FROM pg_stat_activity
-        WHERE pg_stat_activity.datname = '$i'
-        AND pid <> pg_backend_pid();"
-    su postgres -c "psql -U bj -h localhost -d bj -c \"$sql_disconnect\""
-    echo "Dropping database $i"
-    su postgres -c "dropdb $i"
-done
-
-echo "** Restore from ${dumpPath} to ${db}"
-
-# create db
 echo " * Create new ${db} database"
-
-su - postgres -c 'createdb --owner=${user} --encoding=UTF-8 ${db}'
-
-su - postgres -c 'psql ${db} <<EOF
-CREATE LANGUAGE plpgsql;
-EOF'
-
-su - postgres -c 'psql ${db} <<EOF
-ALTER DATABASE ${db} SET TIMEZONE='GMT'
-EOF'
+sudo -Hu postgres createdb --owner="${user}" --encoding=UTF-8 "${db}"
+sudo -Hu postgres psql -d "${db}" -c "CREATE OR REPLACE LANGUAGE plpgsql"
+sudo -Hu postgres psql -d "${db}" -c "ALTER DATABASE ${db} SET TIMEZONE='GMT'"
 
 echo " * Loading ${dumpPath}..."
-PGPASSWORD=${pass} pg_restore -l  ${dumpPath} | grep -v -E '${excludeData}' > /tmp/restore.list
+pg_restore -l ${dumpPath} | grep -v -E '${excludeData}' > /tmp/restore.list
 PGPASSWORD=${pass} pg_restore -U ${user} -h localhost -d ${db} -L /tmp/restore.list ${dumpPath} 2>&1
 
 echo "** Restore complete. Proceed with upgrade..."
