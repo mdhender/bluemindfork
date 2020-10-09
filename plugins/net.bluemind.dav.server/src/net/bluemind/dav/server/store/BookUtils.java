527c80bc3aacb988ee353d0d9f510b239d084386
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2020-10-09 10:17:45 +0200

    BM-16431 Chore: add carddav log

--- plugins/net.bluemind.dav.server/src/net/bluemind/dav/server/store/BookUtils.java
+++ plugins/net.bluemind.dav.server/src/net/bluemind/dav/server/store/BookUtils.java
@@ -2,16 +2,27 @@ package net.bluemind.dav.server.store;
 
 import java.util.regex.Matcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.bluemind.core.container.model.ContainerDescriptor;
 
 public final class BookUtils {
+
+	private BookUtils() {
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(BookUtils.class);
 
 	public static final ContainerDescriptor addressbook(LoggedCore lc, DavResource dr) {
 		ResType rt = dr.getResType();
 		Matcher m = rt.matcher(dr.getPath());
 		m.find();
 		String bookUid = m.group(2);
-		return lc.getBooks().get(bookUid);
+
+		ContainerDescriptor ret = lc.getBooks().get(bookUid);
+		logger.info("Lookup of '{}' => {}", bookUid, ret);
+		return ret;
 	}
 
 }
