97b01b6777248c49da401c4f4f589e56c2c53a5f
Mehdi Rande <mehdi.rande@blue-mind.net>
2020-10-15 17:05:51 +0200

    FEATWEBML-1262 Fix: Load new message on refresh

--- ui/vue-apps/mail-webapp/src/store.deprecated/actions/$_Vuebus_mailboxRecordsChanged.js
+++ ui/vue-apps/mail-webapp/src/store.deprecated/actions/$_Vuebus_mailboxRecordsChanged.js
@@ -1,6 +1,6 @@
 import actionTypes from "../../store/actionTypes";
 
-export async function $_Vuebus_mailboxRecordsChanged({ dispatch, rootGetters, rootState }) {
+export async function $_Vuebus_mailboxRecordsChanged({ dispatch, rootState }) {
     const activeFolderKey = rootState.mail.activeFolder;
     if (!activeFolderKey) {
         console.error("this action must not be called if no activeFolderKey is set.");
@@ -10,6 +10,6 @@ export async function $_Vuebus_mailboxRecordsChanged({ dispatch, rootGetters, ro
     const conversationsEnabled = rootState.session.userSettings.mail_thread === "true";
     await dispatch("mail/" + actionTypes.REFRESH_MESSAGE_LIST_KEYS, { folder, conversationsEnabled }, { root: true });
 
-    const sorted = rootState.mail.messageList.messageKeys.filter(key => rootGetters["mail/isLoaded"](key));
-    await dispatch("mail/" + actionTypes.FETCH_MESSAGE_METADATA, { messageKeys: sorted.slice(0, 200) }, { root: true });
+    const sorted = rootState.mail.messageList.messageKeys.slice(0, 200);
+    await dispatch("mail/" + actionTypes.FETCH_MESSAGE_METADATA, { messageKeys: sorted }, { root: true });
 }
