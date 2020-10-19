9ddddce292db2d91abb4f798464678544ef66c26
blandine <blandine.descamps@gmail.com>
2020-10-19 07:22:07 -0700

    FEATWEBML-838 FEATWEBML-1184 : message alert style

--- ui/vue-apps/mail-webapp/src/components/MailComponentAlert.vue
+++ ui/vue-apps/mail-webapp/src/components/MailComponentAlert.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="mail-component-alert shadow-sm z-index-200 bg-surface">
+    <div class="mail-component-alert z-index-200 bg-surface">
         <bm-alert
             show
             variant="warning"
             class="d-flex flex-fill justify-content-between text-dark px-3 mb-0 align-items-center"
         >
             <bm-icon :icon="icon" class="mr-3 d-block fa-lg text-warning align-self-center" />
-            <slot />
+            <div class="d-flex flex-wrap"><slot /></div>
             <bm-button-close class="ml-auto" @click="$emit('close')" />
         </bm-alert>
     </div>
