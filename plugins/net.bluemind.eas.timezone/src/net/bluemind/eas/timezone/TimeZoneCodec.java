fd99aebce8d7b15ad6b4fc56de0bcdf4f49879aa
David Phan <david.phan@blue-mind.net>
2020-10-26 11:22:17 +0100

    [EAS] BM-16478 Fix: Fix tz from Outlook for mobile

--- plugins/net.bluemind.eas.timezone/src/net/bluemind/eas/timezone/TimeZoneCodec.java
+++ plugins/net.bluemind.eas.timezone/src/net/bluemind/eas/timezone/TimeZoneCodec.java
@@ -28,6 +28,10 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.base64.Base64;
 
+/**
+ * See MS-ASDTYPE 2.7.6 TimeZone
+ *
+ */
 public class TimeZoneCodec {
 
 	@SuppressWarnings("unused")
@@ -38,38 +42,58 @@ public class TimeZoneCodec {
 		ByteBuf buf = Base64.decode(base64Buf).order(ByteOrder.LITTLE_ENDIAN);
 		base64Buf.release();
 
+		// Bias (4 bytes): The value of this field is a LONG, as specified in [MS-DTYP]
+		// section 2.2.27. The offset from UTC, in minutes. For example, the bias for
+		// Pacific Time (UTC-8) is 480.
 		int bias = buf.readInt();
+
+		// StandardName (64 bytes): The value of this field is an array of 32 WCHARs, as
+		// specified in [MS- DTYP] section 2.2.60. It contains an optional description
+		// for standard time. Any unused WCHARs in the array MUST be set to 0x0000.
 		ByteBuf uncutString = Unpooled.buffer(64).order(ByteOrder.LITTLE_ENDIAN);
 		buf.readBytes(uncutString);
-		String standardName = asString(uncutString);
+		String standardName = asString(uncutString, 64);
+
+		// StandardDate (16 bytes): The value of this field is a SYSTEMTIME structure,
+		// as specified in [MS- DTYP] section 2.3.13. It contains the date and time when
+		// the transition from DST to standard time occurs.
 		SystemTime standardDate = SystemTime.of(buf);
+
+		// StandardBias (4 bytes): The value of this field is a LONG. It contains the
+		// number of minutes to add to the value of the Bias field during standard time.
 		int standardBias = buf.readInt();
 
+		// DaylightName (64 bytes): The value of this field is an array of 32 WCHARs. It
+		// contains an optional description for DST. Any unused WCHARs in the array MUST
+		// be set to 0x0000.
 		uncutString = Unpooled.buffer(64).order(ByteOrder.LITTLE_ENDIAN);
 		buf.readBytes(uncutString);
-		String daylightName = asString(uncutString);
+		String daylightName = asString(uncutString, 64);
+
+		// DaylightDate (16 bytes): The value of this field is a SYSTEMTIME structure.
+		// It contains the date and time when the transition from standard time to DST
+		// occurs.
 		SystemTime daylightDate = SystemTime.of(buf);
+
+		// DaylightBias (4 bytes): The value of this field is a LONG. It contains the
+		// number of minutes to add to the value of the Bias field during DST.
 		int daylightBias = buf.readInt();
+
 		buf.release();
+
 		return new EASTimeZone(bias, standardName, standardDate, standardBias, daylightName, daylightDate,
 				daylightBias);
 
 	}
 
-	private static String asString(ByteBuf uncutString) {
+	private static String asString(ByteBuf uncutString, int length) {
 		ByteBuf utf16String = Unpooled.buffer().order(ByteOrder.LITTLE_ENDIAN);
-		byte[] out = new byte[2];
-		do {
-			uncutString.readBytes(out);
-			if (out[0] == 0 && out[1] == 0) {
-				break;
-			} else {
-				utf16String.writeBytes(out);
-			}
-		} while (true);
+		byte[] out = new byte[length];
+		uncutString.readBytes(out);
+		utf16String.writeBytes(out);
 		uncutString.release();
 
-		return utf16String.toString(StandardCharsets.UTF_16LE);
+		return utf16String.toString(StandardCharsets.UTF_16LE).trim();
 	}
 
 }
