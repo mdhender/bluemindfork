c86e77e1135205074886d240315ed905cdd93bfd
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2020-11-18 17:23:55 +0100

    Chore: add email rewrites to the test

--- parent/mailbackend/net.bluemind.backend.mail.replica.service.tests/src/net/bluemind/backend/mail/replica/service/tests/RecordUpdatesTests.java
+++ parent/mailbackend/net.bluemind.backend.mail.replica.service.tests/src/net/bluemind/backend/mail/replica/service/tests/RecordUpdatesTests.java
@@ -21,6 +21,9 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,19 +31,21 @@ import org.junit.Test;
 import net.bluemind.backend.mail.api.IMailboxFolders;
 import net.bluemind.backend.mail.api.IMailboxItems;
 import net.bluemind.backend.mail.api.MailboxFolder;
+import net.bluemind.backend.mail.api.MailboxItem;
 import net.bluemind.backend.mail.replica.service.tests.ReplicationEventsRecorder.Hierarchy;
+import net.bluemind.core.api.ListResult;
 import net.bluemind.core.container.model.ItemFlag;
 import net.bluemind.core.container.model.ItemFlagFilter;
 import net.bluemind.core.container.model.ItemValue;
 import net.bluemind.imap.Flag;
 import net.bluemind.imap.FlagsList;
-import net.bluemind.imap.IMAPException;
 
 public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 
 	private ItemValue<MailboxFolder> inbox;
 
-	public static final int CNT = 500;
+	public static final int MESSAGES = 300;
+	public static final int MARK_STEPS = 5;
 
 	public static final ItemFlagFilter unseenFilter = ItemFlagFilter.create().mustNot(ItemFlag.Deleted)
 			.mustNot(ItemFlag.Seen);
@@ -68,7 +73,7 @@ public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 		FlagsList fl = new FlagsList();
 		fl.add(Flag.SEEN);
 		imapAsUser(sc -> {
-			for (int i = 0; i < CNT; i++) {
+			for (int i = 0; i < MESSAGES; i++) {
 				int added = sc.append("INBOX", eml(), fl);
 				assertTrue(added > 0);
 				if (added % 100 == 0) {
@@ -79,7 +84,7 @@ public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 		});
 
 		int count = 0;
-		while (count < CNT) {
+		while (count < MESSAGES) {
 			Thread.sleep(20);
 			count = itemsApi().count(ItemFlagFilter.create().mustNot(ItemFlag.Deleted)).total;
 			System.err.println("Count is at " + count);
@@ -96,7 +101,7 @@ public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 	}
 
 	@Test
-	public void markAsReadThenUnread() throws IMAPException, InterruptedException {
+	public void markAsReadThenUnread() throws InterruptedException {
 		assertNotNull(inbox);
 		IMailboxItems items = itemsApi();
 		assertTrue(items.getVersion() > 0);
@@ -105,16 +110,58 @@ public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 		Thread.sleep(500);
 		updateItems(true, seenFilter);
 
-		int steps = 50;
 		long time = System.currentTimeMillis();
-		for (int i = 0; i < steps; i++) {
-			System.err.println("Starting step " + (i + 1) + " / " + steps);
+		for (int i = 0; i < MARK_STEPS; i++) {
+			System.err.println("Starting step " + (i + 1) + " / " + MARK_STEPS);
 			updateItems(false, unseenFilter);
 			updateItems(true, seenFilter);
 		}
 		time = System.currentTimeMillis() - time;
-		System.err.println("Did " + steps + " steps in " + time + "ms.");
+		System.err.println("Did " + MARK_STEPS + " steps in " + time + "ms.");
 
+	}
+
+	@Test
+	public void markAsReadThenUnreadWithRandomRewrites() throws InterruptedException {
+		assertNotNull(inbox);
+		IMailboxItems items = itemsApi();
+		assertTrue(items.getVersion() > 0);
+
+		updateItems(false, unseenFilter);
+		Thread.sleep(500);
+		updateItems(true, seenFilter);
+
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < MARK_STEPS; i++) {
+			System.err.println("Starting step " + (i + 1) + " / " + MARK_STEPS);
+			updateItems(false, unseenFilter);
+			performRewrites();
+			updateItems(true, seenFilter);
+		}
+		time = System.currentTimeMillis() - time;
+		System.err.println("Did " + MARK_STEPS + " steps in " + time + "ms.");
+
+	}
+
+	private void performRewrites() {
+		IMailboxItems api = itemsApi();
+		long vers = api.getVersion();
+		ListResult<Long> ids = itemsApi().allIds("-deleted", vers, MESSAGES, 0);
+		List<Long> toList = ids.values;
+		Collections.shuffle(toList, ThreadLocalRandom.current());
+		List<Long> sublist = toList.subList(0, Math.min(5, toList.size()));
+		System.err.println("Will rewrite " + sublist);
+
+		String updPrefix = "[U " + System.nanoTime() + "] ";
+		for (long toRewrite : sublist) {
+			ItemValue<MailboxItem> orig = api.getCompleteById(toRewrite);
+			System.err.println("Rewrite " + orig + "...");
+			long time = System.currentTimeMillis();
+			orig.value.body.subject = updPrefix + orig.value.body.subject;
+			api.updateById(toRewrite, orig.value);
+			time = System.currentTimeMillis() - time;
+			System.err.println("Rewritten in " + time + "ms.");
+		}
 	}
 
 	private void updateItems(boolean setSeen, ItemFlagFilter expectedMatch) throws InterruptedException {
@@ -127,12 +174,15 @@ public class RecordUpdatesTests extends AbstractRollingReplicationTests {
 		assertTrue(result);
 		int unseen = itemsApi().count(expectedMatch).total;
 		System.err.println("match: " + unseen);
+		long time = System.currentTimeMillis();
 		int attempt = 0;
-		while (unseen < CNT) {
+		while (unseen < MESSAGES) {
 			Thread.sleep(20L * ++attempt);
 			unseen = itemsApi().count(expectedMatch).total;
 			System.err.println("match: " + unseen);
 		}
+		time = System.currentTimeMillis() - time;
+		System.out.println(MESSAGES + " Marks replicated in " + time + "ms.");
 	}
 
 }
