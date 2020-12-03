c2fe0ffce1f6b9d41f6db34f7ff5278004972656
Anthony Prades <anthony.prades@bluemind.net>
2020-12-03 20:21:07 +0100

    BM-16578 Feat: add proxy support to check ICS URL handler

--- ui/gwt-ui-libs/net.bluemind.ui.gwtcalendar/src/net/bluemind/ui/gwtcalendar/server/IcsUrlCheckHandler.java
+++ ui/gwt-ui-libs/net.bluemind.ui.gwtcalendar/src/net/bluemind/ui/gwtcalendar/server/IcsUrlCheckHandler.java
@@ -19,173 +19,134 @@
  */
 package net.bluemind.ui.gwtcalendar.server;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.asynchttpclient.AsyncCompletionHandlerBase;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig.Builder;
+import org.asynchttpclient.HttpResponseBodyPart;
+import org.asynchttpclient.HttpResponseStatus;
+import org.asynchttpclient.Response;
+import org.asynchttpclient.uri.Uri;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
-import net.bluemind.webmodule.server.NeedVertx;
-import net.fortuna.ical4j.data.CalendarBuilder;
-import net.fortuna.ical4j.data.CalendarParser;
-import net.fortuna.ical4j.data.CalendarParserFactory;
+import io.vertx.core.parsetools.RecordParser;
+import net.bluemind.hornetq.client.MQ;
+import net.bluemind.hornetq.client.MQ.SharedMap;
+import net.bluemind.proxy.support.AHCWithProxy;
 
-public class IcsUrlCheckHandler implements Handler<HttpServerRequest>, NeedVertx {
+public class IcsUrlCheckHandler implements Handler<HttpServerRequest> {
 	private static final Logger logger = LoggerFactory.getLogger(IcsUrlCheckHandler.class);
-	private Vertx vertx;
+
+	private final AtomicReference<SharedMap<String, String>> sysconf = new AtomicReference<>();
+
+	private final Builder ahcDefaultConfig = AHCWithProxy.defaultConfig()
+			.setRequestTimeout((int) TimeUnit.MILLISECONDS.convert(3, TimeUnit.SECONDS))
+			.setPooledConnectionIdleTimeout((int) TimeUnit.MILLISECONDS.convert(2, TimeUnit.SECONDS));
+
+	public IcsUrlCheckHandler() {
+		MQ.init().thenAccept(v -> sysconf.set(MQ.sharedMap("system.configuration")));
+	}
 
 	@Override
 	public void handle(final HttpServerRequest request) {
 		String url = request.params().get("url");
-		connect(request, url);
-	}
 
-	@SuppressWarnings("deprecation")
-	private void connect(final HttpServerRequest request, final String url) {
-		URL urlp;
 		try {
-			urlp = new URL(url);
-		} catch (MalformedURLException e) {
-
-			errorHandler(request, null, null).handle(e);
+			Uri.create(url);
+		} catch (Throwable mue) {
+			errorHandler(request, url).handle(mue);
 			return;
 		}
 
-		HttpClientOptions opts = new HttpClientOptions().setDefaultHost(urlp.getHost()).setKeepAlive(false)
-				.setConnectTimeout(3000);
-		if (urlp.getPort() != -1) {
-			opts.setDefaultPort(urlp.getPort());
-		}
-		if (urlp.getProtocol().equals("https")) {
-			opts.setSsl(true);
-			opts.setTrustAll(true);
-			if (urlp.getPort() == -1) {
-				opts.setDefaultPort(443);
-			}
-		}
-		String fullpath = urlp.getPath();
-		if (urlp.getQuery() != null) {
-			fullpath += "?" + urlp.getQuery();
-		}
+		connect(request, url);
+	}
 
-		HttpClient client = vertx.createHttpClient(opts);
+	private void connect(final HttpServerRequest request, final String url) {
+		AHCWithProxy
+				.build(ahcDefaultConfig,
+						Optional.ofNullable(sysconf.get()).map(SharedMap::asMap).orElse(Collections.emptyMap()))
+				.prepareGet(url).addHeader("content-type", "application/json;charset=UTF-8")
+				.execute(new AsyncCompletionHandlerBase() {
+					private CompletableFuture<String> calName = new CompletableFuture<String>();
 
-		final String fetchUrl = fullpath;
-		File tmpFile = new File(System.getProperty("java.io.tmpdir"), System.currentTimeMillis() + ".ics");
+					@Override
+					public State onStatusReceived(HttpResponseStatus status) throws Exception {
+						if (status.getStatusCode() >= 400) {
+							errorHandler(request, url)
+									.handle(new Exception(String.format("Unable to get ICS from %s: %s (%d)", url,
+											status.getStatusText(), status.getStatusCode())));
+							return State.ABORT;
+						}
 
-		client.getNow(fetchUrl, response -> {
-			logger.info("handler ret url {}", url);
-			if (response.statusCode() == 301) {
-				String location = response.headers().get("Location");
-				logger.debug("ICS url {} has moved permanently to {}", url, location);
-				client.close();
-				connect(request, location);
+						return super.onStatusReceived(status);
+					}
+
+					RecordParser lineSplit = RecordParser.newDelimited("\n", event -> {
+						String line = event.toString();
+						if (line.startsWith("X-WR-CALNAME")) {
+							completeHandler(request, url)
+									.handle(line.substring(line.indexOf(':') + 1).replace("\r", "").trim());
+							calName.complete(null);
+						}
+					});
+
+					@Override
+					public State onBodyPartReceived(HttpResponseBodyPart content) throws Exception {
+						lineSplit.handle(Buffer.buffer(content.getBodyPartBytes()));
+						return calName.isDone() ? State.ABORT : State.CONTINUE;
+					}
+
+					@Override
+					public void onThrowable(Throwable t) {
+						errorHandler(request, url).handle(t);
+					}
+
+					@Override
+					public Response onCompleted(Response response) throws Exception {
+						if (!calName.isDone()) {
+							completeHandler(request, url).handle(null);
+						}
+
+						return response;
+					}
+				});
+	}
+
+	private Handler<String> completeHandler(HttpServerRequest request, String url) {
+		return calName -> {
+			logger.info("Found calendar name '{}' at {}", calName, url);
+			request.response().headers().add("X-Location", url);
+			request.response().setStatusCode(200);
+
+			if (calName == null) {
+				request.response().end();
 			} else {
-				if (response.statusCode() > 301) {
-					logger.debug("Cannot verify ICS url {}:{}:{}", fetchUrl, response.statusCode(),
-							response.statusMessage());
-					request.response().setStatusCode(500);
-					endRequest(request, client);
-				} else {
-					FileWriter out = null;
-					try {
-						out = new FileWriter(tmpFile);
-					} catch (IOException e1) {
-					}
-					BufferedWriter writer = new BufferedWriter(out);
-					request.response().headers().add("X-Location", url);
-					request.response().setStatusCode(200);
-					response.exceptionHandler(errorHandler(request, writer, tmpFile));
-					response.endHandler(h -> {
-						try {
-							writer.close();
-						} catch (IOException e) {
-						}
-						CompletableFuture<Optional<String>> calName = parseCalName(tmpFile);
-						calName.thenAccept(cal -> {
-							if (cal.isPresent()) {
-								request.response().end(cal.get());
-								client.close();
-							} else {
-								endRequest(request, client);
-							}
-							tmpFile.delete();
-						});
-					});
-					response.handler(h -> {
-						try {
-							writer.append(h.toString());
-						} catch (IOException e) {
-
-						}
-					});
-				}
-			}
-		});
-	}
-
-	private void endRequest(final HttpServerRequest request, HttpClient client) {
-		request.response().end();
-		client.close();
-	}
-
-	private CompletableFuture<Optional<String>> parseCalName(File tmpFile) {
-		return CompletableFuture.supplyAsync(() -> {
-			CalendarParser parser = CalendarParserFactory.getInstance().createParser();
-			CalendarBuilder builder = new CalendarBuilder(parser);
-			try (BufferedReader reader = new BufferedReader(
-					new InputStreamReader(Files.newInputStream(tmpFile.toPath())))) {
-				// X-WR-CALNAME
-				String line;
-				while ((line = reader.readLine()) != null) {
-					if (line.contains("X-WR-CALNAME")) {
-						return Optional.of(line.substring(line.indexOf(":") + 1).trim());
-					}
-				}
-			} catch (Exception e) {
-				logger.warn("Cannot parse calendar ics", e);
-			}
-			return Optional.empty();
-		});
-
-	}
-
-	private Handler<Throwable> errorHandler(final HttpServerRequest request, BufferedWriter writer, File tmpFile) {
-		return e -> {
-			logger.error("error during url checking", e);
-			HttpServerResponse resp = request.response();
-			resp.setStatusCode(500);
-			resp.setStatusMessage(e.getMessage() != null ? e.getMessage() : "null");
-			resp.end();
-			if (writer != null) {
-				try {
-					writer.close();
-				} catch (IOException e1) {
-				}
-			}
-			if (tmpFile != null) {
-				tmpFile.delete();
+				request.response().end(calName);
 			}
 		};
 	}
 
-	public void setVertx(Vertx vertx) {
-		this.vertx = vertx;
-	}
+	private Handler<Throwable> errorHandler(HttpServerRequest request, String url) {
+		return e -> {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Error during URL checking: {}", url, e);
+			} else {
+				logger.error("Error during URL checking: {}", url);
+			}
 
+			HttpServerResponse resp = request.response();
+			resp.setStatusCode(500);
+			resp.setStatusMessage(e.getMessage() != null ? e.getMessage() : "null");
+			resp.end();
+		};
+	}
 }
