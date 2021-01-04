193b5f82f0970b6fe30442c9690a1f73c75c3b4e
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2021-01-04 15:29:40 +0100

    [sds] BM-16230 Fix: last file chunk might not be flushed when reading from s3

--- parent/cloud/net.bluemind.core.sds.replication/src/net/bluemind/core/sds/replication/S3ObjectStoreReader.java
+++ parent/cloud/net.bluemind.core.sds.replication/src/net/bluemind/core/sds/replication/S3ObjectStoreReader.java
@@ -18,11 +18,16 @@
 package net.bluemind.core.sds.replication;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +37,8 @@ import com.netflix.spectator.api.Registry;
 import net.bluemind.backend.mail.replica.service.sds.IObjectStoreReader;
 import net.bluemind.core.api.fault.ServerFault;
 import net.bluemind.metrics.registry.IdFactory;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
+import software.amazon.awssdk.core.async.SdkPublisher;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
@@ -69,20 +76,106 @@ public class S3ObjectStoreReader implements IObjectStoreReader {
 		Path target = null;
 		try {
 			target = Files.createTempFile(guid, ".s3");
-			Files.delete(target);
 		} catch (IOException e1) {
 			throw new ServerFault(e1);
 		}
 		try {
 			final Path notNull = target;
-			return client.getObject(GetObjectRequest.builder().bucket(bucket).key(guid).build(), target)
-					.thenApply(v -> {
-						registry.distributionSummary(idFactory.name("read")).record(notNull.toFile().length());
+			return client.getObject(GetObjectRequest.builder().bucket(bucket).key(guid).build(),
+					new SimpleDownloader<>(target)).thenApply(v -> {
+						registry.distributionSummary(idFactory.name("read")).record(v.contentLength());
 						return notNull;
 					}).get(10, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			throw new ServerFault(e);
 		}
+	}
+
+	private static class SimpleDownloader<T> implements AsyncResponseTransformer<T, T> {
+
+		private final Path path;
+		private CompletableFuture<T> cf;
+		private T response;
+		private long transferred;
+		private SeekableByteChannel channel;
+
+		public SimpleDownloader(Path path) {
+			this.path = path;
+		}
+
+		@Override
+		public CompletableFuture<T> prepare() {
+			cf = new CompletableFuture<>();
+			try {
+				this.channel = Files.newByteChannel(path, StandardOpenOption.CREATE,
+						StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+			} catch (IOException e) {
+				cf.completeExceptionally(e);
+			}
+
+			return cf.thenApply(v -> response);
+		}
+
+		@Override
+		public void onResponse(T response) {
+			this.response = response;
+		}
+
+		@Override
+		public void onStream(SdkPublisher<ByteBuffer> publisher) {
+			publisher.subscribe(new Subscriber<ByteBuffer>() {
+
+				private Subscription sub;
+
+				@Override
+				public void onSubscribe(Subscription s) {
+					sub = s;
+					sub.request(1);
+				}
+
+				@Override
+				public void onNext(ByteBuffer t) {
+					transferred += t.remaining();
+					try {
+						channel.write(t);
+						sub.request(1);
+					} catch (IOException e) {
+						exceptionOccurred(e);
+					}
+				}
+
+				@Override
+				public void onError(Throwable t) {
+					exceptionOccurred(t);
+				}
+
+				@Override
+				public void onComplete() {
+					logger.debug("Completed of {} byte(s) transferred", transferred);
+					try {
+						channel.close();
+						channel = null;
+					} catch (IOException e) {
+					}
+					cf.complete(response);
+				}
+			});
+
+		}
+
+		@Override
+		public void exceptionOccurred(Throwable error) {
+			try {
+				if (channel != null) {
+					channel.close();
+				}
+				Files.deleteIfExists(path);
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+			cf.completeExceptionally(error);
+		}
+
 	}
 
 	@Override
@@ -91,7 +184,6 @@ public class S3ObjectStoreReader implements IObjectStoreReader {
 		for (int i = 0; i < guids.length; i++) {
 			try {
 				ret[i] = Files.createTempFile(guids[i], ".s3");
-				Files.delete(ret[i]);
 			} catch (IOException e1) {
 				throw new ServerFault(e1);
 			}
@@ -100,12 +192,8 @@ public class S3ObjectStoreReader implements IObjectStoreReader {
 		DistributionSummary distSum = registry.distributionSummary(idFactory.name("read"));
 		for (int i = 0; i < guids.length; i++) {
 			final Path cur = ret[i];
-			dls[i] = client.getObject(GetObjectRequest.builder().bucket(bucket).key(guids[i]).build(), cur)
-					.thenAccept(v -> {
-						if (Files.exists(cur)) {
-							distSum.record(cur.toFile().length());
-						}
-					});
+			dls[i] = client.getObject(GetObjectRequest.builder().bucket(bucket).key(guids[i]).build(),
+					new SimpleDownloader<>(cur)).thenAccept(v -> distSum.record(v.contentLength()));
 		}
 		try {
 			CompletableFuture.allOf(dls).get(15, TimeUnit.SECONDS);
