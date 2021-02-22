d24b48f8e0054f714c382bb54c746962aacf318b
kladier <kevin.ladier@bluemind.net>
2021-02-22 14:06:10 +0100

    FEATWEBML-1487 Fix: add securityContext param to CleanTemporaryParts

--- parent/mailbackend/net.bluemind.backend.mail.replica.service/src/net/bluemind/backend/mail/replica/service/internal/hooks/CleanTemporaryParts.java
+++ parent/mailbackend/net.bluemind.backend.mail.replica.service/src/net/bluemind/backend/mail/replica/service/internal/hooks/CleanTemporaryParts.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.bluemind.backend.mail.parsing.Bodies;
+import net.bluemind.core.context.SecurityContext;
 import net.bluemind.core.sessions.ISessionDeletionListener;
 
 public class CleanTemporaryParts implements ISessionDeletionListener {
@@ -13,7 +14,7 @@ public class CleanTemporaryParts implements ISessionDeletionListener {
 	private static final Logger logger = LoggerFactory.getLogger(CleanTemporaryParts.class);
 
 	@Override
-	public void deleted(String identity, String sid) {
+	public void deleted(String identity, String sid, SecurityContext securityContext) {
 
 		File sidFolder = Bodies.getFolder(sid);
 
