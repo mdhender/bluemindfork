b88c50e63dbf3c0b888024c8174ad565639e2fb2
kladier <kevin.ladier@bluemind.net>
2021-04-27 17:08:26 +0200

    FEATBL-1316 Feat: add status information when resetting local data

--- ui/vue-apps/root-webapp/src/components/preferences/fields/customs/PrefResetLocalData.vue
+++ ui/vue-apps/root-webapp/src/components/preferences/fields/customs/PrefResetLocalData.vue
@@ -1,32 +1,48 @@
 <template>
     <div>
         <div class="mb-2">{{ options.label }}</div>
-        <bm-button variant="outline-warning" @click="resetLocalData">{{ options.text }}</bm-button>
+        <bm-button v-if="status === 'IDLE'" variant="outline-warning" @click="resetLocalData">
+            {{ options.text }}
+        </bm-button>
+        <template v-else-if="status === 'LOADING'">
+            <bm-spinner class="d-inline" :size="0.3" /> {{ $t("preferences.advanced.reinit_local_data.in_progress") }}
+        </template>
+        <bm-label-icon v-else-if="status === 'SUCCESS'" class="text-success" icon="check-circle">
+            {{ $t("preferences.advanced.reinit_local_data.success") }}
+        </bm-label-icon>
+        <bm-label-icon v-else-if="status === 'ERROR'" class="text-danger" icon="info-circle-plain">
+            {{ $t("preferences.advanced.reinit_local_data.error") }}
+        </bm-label-icon>
     </div>
 </template>
 
 <script>
 import { inject } from "@bluemind/inject";
-import { BmButton } from "@bluemind/styleguide";
+import { BmButton, BmLabelIcon, BmSpinner } from "@bluemind/styleguide";
 
 import PrefAlertsMixin from "../../mixins/PrefAlertsMixin";
 import PrefFieldMixin from "../../mixins/PrefFieldMixin";
 
 export default {
     name: "PrefResetLocalData",
-    components: {
-        BmButton
-    },
+    components: { BmButton, BmLabelIcon, BmSpinner },
     mixins: [PrefAlertsMixin, PrefFieldMixin],
+    data() {
+        return { status: "IDLE" };
+    },
     methods: {
         async resetLocalData() {
+            this.status = "LOADING";
+
             // LocalStorage
             localStorage.clear();
 
             // Cache API
             const baseUrl = location.protocol + "//" + location.hostname + "/webapp/";
             const cacheNames = ["css-cache", "part-cache", "bm-assets", "workbox-runtime-" + baseUrl];
-            cacheNames.map(name => caches.delete(name));
+            await Promise.all(cacheNames.map(name => caches.delete(name))).catch(() => {
+                this.status = "ERROR";
+            });
 
             // IndexedDB
             const { userId, domain } = inject("UserSession");
@@ -44,12 +60,20 @@ export default {
                 "deferredaction",
                 newWebmailDbName
             ];
+            let successfullDeletions = 0;
             dbNames.forEach(name => {
                 const deleteDBRequest = indexedDB.deleteDatabase(name);
-                deleteDBRequest.onerror = () => indexedDB.deleteDatabase(name);
+                deleteDBRequest.onerror = () => {
+                    this.status = "ERROR";
+                };
+                deleteDBRequest.onsuccess = () => {
+                    successfullDeletions++;
+                    if (successfullDeletions === dbNames.length && this.status !== "ERROR") {
+                        this.status = "SUCCESS";
+                        this.showReloadAppAlert();
+                    }
+                };
             });
-
-            this.showReloadAppAlert();
         }
     }
 };
