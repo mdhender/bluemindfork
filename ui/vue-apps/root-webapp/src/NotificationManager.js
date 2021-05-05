36eba22aaa08a30db88d83902369a305d83b40b8
Laurent Coustet <laurent.coustet@bluemind.net>
2021-05-05 16:25:31 +0200

    [root-webapp] chore: allow NotificationManager not to crash when no webmail application is present

--- ui/vue-apps/root-webapp/src/NotificationManager.js
+++ ui/vue-apps/root-webapp/src/NotificationManager.js
@@ -36,6 +36,10 @@ export default class NotificationManager {
             const mailAppExtension = window.bmExtensions_["net.bluemind.banner"].find(
                 extension => extension.application.role === "hasMail" && extension.application.href.includes("mail")
             );
+            if (! mailAppExtension) {
+                console.log("sorry, no mailApp registered");
+                return;
+            }
             const mailIconAsSvg = mailAppExtension.application.children["icon-svg"].body;
             const mailIconAsBlobURL = URL.createObjectURL(new Blob([mailIconAsSvg], { type: "image/svg+xml" }));
 
