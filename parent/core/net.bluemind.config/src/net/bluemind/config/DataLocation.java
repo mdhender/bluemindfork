50dac3f6f98c7796c7e8410fcd24ba77e07ca437
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2021-05-18 09:30:57 +0200

    Chore: ensure we sometime recheck if /etc/bm/server.uid exists

--- parent/core/net.bluemind.config/src/net/bluemind/config/DataLocation.java
+++ parent/core/net.bluemind.config/src/net/bluemind/config/DataLocation.java
@@ -20,27 +20,44 @@ package net.bluemind.config;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Suppliers;
 import com.google.common.io.Files;
 
 public class DataLocation {
 
-	private static final String dataLoc = currentImpl();
+	private static final Logger logger = LoggerFactory.getLogger(DataLocation.class);
+	private static final String PATH = "/etc/bm/server.uid";
+
+	private static final Supplier<String> dataLoc = Suppliers.memoizeWithExpiration(DataLocation::currentImpl, 1,
+			TimeUnit.MINUTES);
+
+	private DataLocation() {
+	}
 
 	/**
-	 * Returns the data location of the running BlueMind JVM
+	 * Returns the data location of the running BlueMind JVM, based on
+	 * <code>/etc/bm/server.uid</code>
+	 * 
+	 * <code>unknown</code> is returned if the file is missing.
 	 * 
 	 * @return
 	 */
 	public static String current() {
-		return dataLoc;
+		return dataLoc.get();
 	}
 
 	private static String currentImpl() {
 		try {
-			return Files.asCharSource(new File("/etc/bm/server.uid"), StandardCharsets.US_ASCII).read();
+			return Files.asCharSource(new File(PATH), StandardCharsets.US_ASCII).read();
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			logger.warn("Error figuring out current location ({}) => unknown", e.getMessage());
+			return "unknown";
 		}
 	}
 
