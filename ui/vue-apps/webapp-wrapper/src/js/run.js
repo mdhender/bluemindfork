20aa593249165f663bff9e01ac6eeda7c207a325
Mehdi Rande <mehdi.rande@blue-mind.net>
2021-06-17 17:04:59 +0200

    FEATBL-1439 Fix: redirect to index.html in vue-router too

--- ui/vue-apps/webapp-wrapper/src/js/run.js
+++ ui/vue-apps/webapp-wrapper/src/js/run.js
@@ -5,6 +5,7 @@ import ApplicationWrapper from "./ApplicationWrapper.vue";
 
 Vue.component("application-wrapper", ApplicationWrapper);
 router.addRoutes([
+    { path: "/", redirect: "/index.html" },
     {
         name: "wrapper:root",
         path: "/index.html",
