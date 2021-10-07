124b141ec437a5cba4f4d19201bc6db3a9b6d25f
kladier <kevin.ladier@bluemind.net>
2021-10-07 16:51:03 +0200

    FEATWEBML-1724 [MailApp] Fix: 'this' was missing to make refresh list works

--- ui/vue-apps/mail-webapp/src/components/MailConversationList/MailConversationList.vue
+++ ui/vue-apps/mail-webapp/src/components/MailConversationList/MailConversationList.vue
@@ -51,7 +51,7 @@ export default {
     },
     bus: {
         [PUSHED_FOLDER_CHANGES]: function (folderUid) {
-            if (!CONVERSATION_LIST_IS_SEARCH_MODE && this.folders[this.activeFolder].remoteRef.uid === folderUid) {
+            if (!this.CONVERSATION_LIST_IS_SEARCH_MODE && this.folders[this.activeFolder].remoteRef.uid === folderUid) {
                 this.refreshList();
             }
         }
