76f701c8e38ed9ce6e32d8fdc9a01100b26077ed
Vincent Vignaud <vincent.vignaud@bluemind.net>
2021-10-25 11:59:49 +0200

    FEATBL-1557 [MailApp] Fix: update date separators when a new day has come

---
+++ ui/vue-apps/mail-webapp/src/components/ConversationList/DateRanges.js
@@ -0,0 +1,50 @@
+import { DateRange } from "@bluemind/date";
+
+export default class DateRanges {
+    future = DateRange.future();
+    today = DateRange.today();
+    yesterday = DateRange.yesterday();
+    thisWeek = DateRange.thisWeek();
+    lastWeek = DateRange.lastWeek();
+    thisMonth = DateRange.thisMonth();
+    lastMonth = DateRange.lastMonth();
+    monthsBeforeLastMonth = DateRange.pastMonths()
+        .slice(1)
+        .map(range => {
+            range.date = range.start;
+            range.dateFormat = "month";
+            return range;
+        });
+    pastYears = DateRange.pastYears().map(range => {
+        range.text = String(range.start.getFullYear());
+        return range;
+    });
+    older;
+    sortedArray;
+
+    constructor() {
+        this.future.i18n = "mail.list.range.future";
+        this.today.i18n = "mail.list.range.today";
+        this.yesterday.i18n = "mail.list.range.yesterday";
+        this.thisWeek.end = this.yesterday.start;
+        this.thisWeek.i18n = "mail.list.range.this_week";
+        this.lastWeek.i18n = "mail.list.range.last_week";
+        this.thisMonth.end = this.lastWeek.start;
+        this.thisMonth.i18n = "mail.list.range.this_month";
+        this.lastMonth.i18n = "mail.list.range.last_month";
+        this.older = DateRange.past(this.pastYears[this.pastYears.length - 1].start);
+        this.older.i18n = "mail.list.range.older";
+        this.sortedArray = [
+            this.future,
+            this.today,
+            this.yesterday,
+            this.thisWeek,
+            this.lastWeek,
+            this.thisMonth,
+            this.lastMonth,
+            ...this.monthsBeforeLastMonth,
+            ...this.pastYears,
+            this.older
+        ];
+    }
+}
--- ui/vue-apps/mail-webapp/src/components/ConversationList/DateSeparator.vue
+++ ui/vue-apps/mail-webapp/src/components/ConversationList/DateSeparator.vue
@@ -2,7 +2,7 @@
     <conversation-list-separator v-if="needSeparator" class="date-separator" :text="text" />
 </template>
 <script>
-import { DateRange } from "@bluemind/date";
+import DateRanges from "./DateRanges";
 import ConversationListSeparator from "./ConversationListSeparator";
 
 export default {
@@ -35,19 +35,30 @@ export default {
     }
 };
 
-let allSeparators = [];
+let allSeparators;
 let dateRangeIndex;
 let separatorAdded;
+let dateRanges;
 
 export function init() {
     allSeparators = [];
     dateRangeIndex = undefined;
     separatorAdded = undefined;
+    dateRanges = new DateRanges();
+}
+
+/** If a new day has come we must update date ranges. */
+function checkNewDay() {
+    if (!dateRanges.today.contains(new Date())) {
+        dateRanges = new DateRanges();
+    }
 }
 
 function computeVisibilities() {
     dateRangeIndex = 0;
     separatorAdded = false;
+    checkNewDay();
+
     let index = 0;
     let dateSeparator = allSeparators[index];
     while (dateSeparator) {
@@ -59,15 +70,15 @@ function computeVisibilities() {
 function computeVisibility(dateSeparator) {
     dateSeparator.needSeparator = false;
 
-    while (!DATE_RANGES[dateRangeIndex].contains(dateSeparator.conversation.date)) {
+    while (!dateRanges.sortedArray[dateRangeIndex].contains(dateSeparator.conversation.date)) {
         separatorAdded = false;
         dateRangeIndex++;
-        if (dateRangeIndex === DATE_RANGES.length) {
+        if (dateRangeIndex === dateRanges.length) {
             dateRangeIndex = 0;
         }
     }
 
-    const range = DATE_RANGES[dateRangeIndex];
+    const range = dateRanges.sortedArray[dateRangeIndex];
     if (!separatorAdded || dateSeparator.index === 0) {
         dateSeparator.text = range.i18n
             ? dateSeparator.$t(range.i18n)
@@ -86,47 +97,4 @@ function computeVisibility(dateSeparator) {
         }
     }
 }
-
-/** Warning: keep these ranges mutually exclusives for the global dateRangeIndex to work correctly. */
-const FUTURE = DateRange.future();
-FUTURE.i18n = "mail.list.range.future";
-const TODAY = DateRange.today();
-TODAY.i18n = "mail.list.range.today";
-const YESTERDAY = DateRange.yesterday();
-YESTERDAY.i18n = "mail.list.range.yesterday";
-const THIS_WEEK = DateRange.thisWeek();
-THIS_WEEK.end = YESTERDAY.start;
-THIS_WEEK.i18n = "mail.list.range.this_week";
-const LAST_WEEK = DateRange.lastWeek();
-LAST_WEEK.i18n = "mail.list.range.last_week";
-const THIS_MONTH = DateRange.thisMonth();
-THIS_MONTH.end = LAST_WEEK.start;
-THIS_MONTH.i18n = "mail.list.range.this_month";
-const LAST_MONTH = DateRange.lastMonth();
-LAST_MONTH.i18n = "mail.list.range.last_month";
-const MONTHS_BEFORE_LAST_MONTH = DateRange.pastMonths()
-    .slice(1)
-    .map(range => {
-        range.date = range.start;
-        range.dateFormat = "month";
-        return range;
-    });
-const PAST_YEARS = DateRange.pastYears().map(range => {
-    range.text = String(range.start.getFullYear());
-    return range;
-});
-const OLDER = DateRange.past(PAST_YEARS[PAST_YEARS.length - 1].start);
-OLDER.i18n = "mail.list.range.older";
-const DATE_RANGES = [
-    FUTURE,
-    TODAY,
-    YESTERDAY,
-    THIS_WEEK,
-    LAST_WEEK,
-    THIS_MONTH,
-    LAST_MONTH,
-    ...MONTHS_BEFORE_LAST_MONTH,
-    ...PAST_YEARS,
-    OLDER
-];
 </script>
