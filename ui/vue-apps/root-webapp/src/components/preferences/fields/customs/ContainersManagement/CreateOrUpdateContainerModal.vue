ccfc29bd177902b24d5b95364027ff4c5a0eff31
Vincent Vignaud <vincent.vignaud@bluemind.net>
2022-02-02 10:28:27 +0100

    BM-18133 Fix: preferences - container modal save issue

--- ui/vue-apps/root-webapp/src/components/preferences/fields/customs/ContainersManagement/CreateOrUpdateContainerModal.vue
+++ ui/vue-apps/root-webapp/src/components/preferences/fields/customs/ContainersManagement/CreateOrUpdateContainerModal.vue
@@ -9,7 +9,7 @@
         :ok-disabled="disableSave"
         modal-class="create-or-update-container-modal"
         body-class="row mt-3"
-        @ok="save"
+        @ok.prevent="save"
     >
         <div class="col-2"><bm-icon :icon="containerIcon" size="3x" class="mt-3" /></div>
         <bm-form class="col-10" @submit.prevent="save">
