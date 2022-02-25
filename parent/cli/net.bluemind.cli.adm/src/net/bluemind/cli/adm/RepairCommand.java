af02b5d47e76f79fbce41bcb11977d614da4350d
Thomas Fricker <thomas.fricker@blue-mind.net>
2022-02-25 11:53:14 +0100

    Feat: add option to repair archived users, improve logging

--- parent/cli/net.bluemind.cli.adm/src/net/bluemind/cli/adm/RepairCommand.java
+++ parent/cli/net.bluemind.cli.adm/src/net/bluemind/cli/adm/RepairCommand.java
@@ -29,11 +29,17 @@ import net.bluemind.cli.cmd.api.ICmdLetRegistration;
 import net.bluemind.cli.directory.common.NoopException;
 import net.bluemind.cli.directory.common.SingleOrDomainOperation;
 import net.bluemind.cli.utils.Tasks;
+import net.bluemind.core.api.report.DiagnosticReport;
+import net.bluemind.core.api.report.DiagnosticReport.State;
 import net.bluemind.core.container.model.ItemValue;
 import net.bluemind.core.task.api.TaskRef;
+import net.bluemind.core.task.api.TaskStatus;
+import net.bluemind.core.utils.JsonUtils;
 import net.bluemind.directory.api.BaseDirEntry.Kind;
 import net.bluemind.directory.api.DirEntry;
 import net.bluemind.directory.api.IDirEntryMaintenance;
+import net.bluemind.user.api.IUser;
+import net.bluemind.user.api.User;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -60,8 +66,33 @@ public class RepairCommand extends SingleOrDomainOperation {
 	@Option(names = "--ops", description = "Just include the (comma separated) ops, (eg. mailboxPostfixMaps,)")
 	public String ops;
 
+	@Option(names = "--unarchive", description = "\"true\" to temporarely unarchive/archive users and apply the repair op")
+	public boolean unarchive = false;
+
 	@Override
 	public void synchronousDirOperation(String domainUid, ItemValue<DirEntry> de) {
+		IUser userService = null;
+		ItemValue<User> userItem = null;
+
+		boolean archived = unarchive && de.value.kind == Kind.USER && de.value.archived;
+		try {
+			if (archived) {
+				ctx.info("User " + de.value.entryUid + " will be unarchived for repair op");
+				userService = ctx.adminApi().instance(IUser.class, domainUid);
+				userItem = userService.getComplete(de.value.entryUid);
+				unarchive(userService, userItem);
+			}
+
+			repair(domainUid, de);
+		} finally {
+			if (archived && userService != null && userItem != null) {
+				ctx.info("User " + de.value.entryUid + " will be archived");
+				archive(userService, userItem);
+			}
+		}
+	}
+
+	private void repair(String domainUid, ItemValue<DirEntry> de) {
 		IDirEntryMaintenance demService = ctx.adminApi().instance(IDirEntryMaintenance.class, domainUid, de.uid);
 		Set<String> opsIds = demService.getAvailableOperations().stream().map(mo -> mo.identifier)
 				.collect(Collectors.toSet());
@@ -79,7 +110,25 @@ public class RepairCommand extends SingleOrDomainOperation {
 		}
 
 		TaskRef ref = dry ? demService.check(filteredOps) : demService.repair(filteredOps);
-		Tasks.follow(ctx, ref, String.format("Fail to repair entry %s", de));
+		TaskStatus status = Tasks.follow(ctx, ref, String.format("Failed to repair entry %s", de));
+		if (!status.state.succeed) {
+			DiagnosticReport report = JsonUtils.read(status.result, DiagnosticReport.class);
+			report.entries.stream().filter(e -> e.state == State.KO).forEach(e -> ctx.error(e.toString()));
+		}
+	}
+
+	private void unarchive(IUser userService, ItemValue<User> userItem) {
+		userItem.value.archived = false;
+		updateUser(userService, userItem);
+	}
+
+	private void archive(IUser userService, ItemValue<User> userItem) {
+		userItem.value.archived = true;
+		updateUser(userService, userItem);
+	}
+
+	private void updateUser(IUser userService, ItemValue<User> userItem) {
+		userService.update(userItem.uid, userItem.value);
 	}
 
 	@Override
