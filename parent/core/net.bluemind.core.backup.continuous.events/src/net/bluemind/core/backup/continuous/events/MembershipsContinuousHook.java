b6df6ffd79baf448f8b6311da8ae3efcb54663c2
Elie Prudhomme <elie.prudhomme@bluemind.net>
2022-03-15 10:53:13 +0100

    BM-18331 Fix: don't backup memberships on group deletion

--- parent/core/net.bluemind.core.backup.continuous.events/src/net/bluemind/core/backup/continuous/events/MembershipsContinuousHook.java
+++ parent/core/net.bluemind.core.backup.continuous.events/src/net/bluemind/core/backup/continuous/events/MembershipsContinuousHook.java
@@ -21,10 +21,7 @@ import java.util.List;
 
 import net.bluemind.core.api.fault.ServerFault;
 import net.bluemind.core.backup.continuous.dto.GroupMembership;
-import net.bluemind.core.context.SecurityContext;
-import net.bluemind.core.rest.ServerSideServiceProvider;
 import net.bluemind.group.api.Group;
-import net.bluemind.group.api.IGroup;
 import net.bluemind.group.api.Member;
 import net.bluemind.group.hook.GroupMessage;
 import net.bluemind.group.hook.IGroupHook;
@@ -46,10 +43,6 @@ public class MembershipsContinuousHook implements IGroupHook, ContinuousContenai
 
 	@Override
 	public void onGroupDeleted(GroupMessage deleted) throws ServerFault {
-		ServerSideServiceProvider prov = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM);
-		IGroup grpApi = prov.instance(IGroup.class, deleted.container.domainUid);
-		List<Member> fullList = grpApi.getMembers(deleted.group.uid);
-		saveMembers(deleted, fullList, false);
 	}
 
 	@Override
