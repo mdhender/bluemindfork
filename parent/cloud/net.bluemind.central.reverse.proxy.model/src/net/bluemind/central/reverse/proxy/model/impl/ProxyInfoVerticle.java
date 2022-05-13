8e1ec2859ce3d33e5c2fc4663a79bbe04897b65d
Laurent Coustet <laurent.coustet@bluemind.net>
2022-05-13 15:35:19 +0200

    [bm-crp] fix: don't try to use multiple consumer, while using a poll, with a in-order executeBlocking queue

--- parent/cloud/net.bluemind.central.reverse.proxy.model/src/net/bluemind/central/reverse/proxy/model/impl/ProxyInfoVerticle.java
+++ parent/cloud/net.bluemind.central.reverse.proxy.model/src/net/bluemind/central/reverse/proxy/model/impl/ProxyInfoVerticle.java
@@ -44,13 +44,10 @@ public class ProxyInfoVerticle extends AbstractVerticle {
 		String bootstrapServers = kafkaBootstrapServers()
 				.orElseThrow(() -> new RuntimeException("No configuration available for kafka bootstrap server"));
 		KafkaAdminClient adminClient = KafkaAdminClient.create(bootstrapServers);
-//		Context context = vertx.getOrCreateContext();
 		adminClient.listTopics().map(InstallationTopics::new).onSuccess(installationTopics -> {
-//			context.runOnContext((v) -> {
 			logger.info("Subscribing to {}", installationTopics.domainTopics);
 			setupConsumer(bootstrapServers, installationTopics);
 			p.complete();
-//			});
 		}).onFailure(t -> logger.error("Unable to list installation topic names", t));
 	}
 
@@ -60,13 +57,10 @@ public class ProxyInfoVerticle extends AbstractVerticle {
 
 	private void setupConsumer(String bootstrapServers, InstallationTopics topics) {
 		List<String> topicNames = topicNamesToConsume(topics);
-		for (int i = 0; i < 8; i++) {
-			String groupIdInstance = "consumer-" + i;
-			KafkaConsumerClient<byte[], byte[]> consumer = createConsumer(bootstrapServers, groupIdInstance);
-			consumers.add(consumer);
-			consumer.handler(recordHandler).subscribe(topicNames);
-			logger.info("consumer {} subscibed to {}", groupIdInstance, topicNames);
-		}
+		KafkaConsumerClient<byte[], byte[]> consumer = createConsumer(bootstrapServers, CONSUMER_GROUP_NAME);
+		consumers.add(consumer);
+		consumer.handler(recordHandler).subscribe(topicNames);
+		logger.info("consumer {} subscibed to {}", CONSUMER_GROUP_NAME, topicNames);
 	}
 
 	private KafkaConsumerClient<byte[], byte[]> createConsumer(String bootstrapServers, String groupInstanceId) {
@@ -74,7 +68,7 @@ public class ProxyInfoVerticle extends AbstractVerticle {
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_NAME);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupInstanceId);
 		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
 
