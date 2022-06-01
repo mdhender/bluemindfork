8eacf4644ce2999c44421631c65e2e7af7f0eaa7
Thomas Fricker <thomas.fricker@blue-mind.net>
2022-06-01 15:53:38 +0200

    Revert "FEATBL-1750 Fix: list operations by kind instead of printing an enormous repeating list"
    This reverts commit ee573782316b79cea3132838e7975e74e7e7de8a.

--- parent/cli/net.bluemind.cli.directory.common/src/net/bluemind/cli/directory/common/SingleOrDomainOperation.java
+++ parent/cli/net.bluemind.cli.directory.common/src/net/bluemind/cli/directory/common/SingleOrDomainOperation.java
@@ -70,10 +70,6 @@ public abstract class SingleOrDomainOperation implements ICmdLet, Runnable {
 
 	public abstract Kind[] getDirEntryKind();
 
-	public void done() {
-
-	}
-
 	@Override
 	public Runnable forContext(CliContext ctx) {
 		this.ctx = ctx;
@@ -142,7 +138,6 @@ public abstract class SingleOrDomainOperation implements ICmdLet, Runnable {
 			int handled = entriesWithDomainUid.size() - noops;
 			ctx.warn("Handled " + handled + " entries. " + noops + " entries have been ignored");
 		}
-		done();
 	}
 
 	/**
