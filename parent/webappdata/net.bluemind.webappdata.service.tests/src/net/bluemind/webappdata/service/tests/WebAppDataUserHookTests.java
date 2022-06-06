a0aa55465adcfbf81322cf47be1bb729ffa58cc7
kladier <kevin.ladier@bluemind.net>
2022-06-06 16:30:07 +0200

    FEATWEBML-2130 [WebAppData] Feat: manage user webappdata container lifecycle

---
+++ parent/webappdata/net.bluemind.webappdata.service.tests/src/net/bluemind/webappdata/service/tests/WebAppDataUserHookTests.java
@@ -0,0 +1,96 @@
+/* BEGIN LICENSE
+ * Copyright © Blue Mind SAS, 2012-2022
+ *
+ * This file is part of BlueMind. BlueMind is a messaging and collaborative
+ * solution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See LICENSE.txt
+ * END LICENSE
+ */
+package net.bluemind.webappdata.service.tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.bluemind.core.container.api.IContainers;
+import net.bluemind.core.context.SecurityContext;
+import net.bluemind.core.jdbc.JdbcTestHelper;
+import net.bluemind.core.rest.ServerSideServiceProvider;
+import net.bluemind.core.task.service.TaskUtils;
+import net.bluemind.core.utils.UIDGenerator;
+import net.bluemind.lib.vertx.VertxPlatform;
+import net.bluemind.tests.defaultdata.PopulateHelper;
+import net.bluemind.user.api.IUser;
+import net.bluemind.webappdata.api.IWebAppData;
+import net.bluemind.webappdata.api.IWebAppDataUids;
+import net.bluemind.webappdata.api.WebAppData;
+
+public class WebAppDataUserHookTests {
+
+	private String domainUid;
+
+	@Before
+	public void setup() throws Exception {
+		JdbcTestHelper.getInstance().beforeTest();
+		final CountDownLatch launched = new CountDownLatch(1);
+		VertxPlatform.spawnVerticles(event -> launched.countDown());
+		launched.await();
+
+		PopulateHelper.initGlobalVirt();
+
+		domainUid = "dom" + System.currentTimeMillis() + ".test";
+		domainUid = PopulateHelper.createTestDomain(domainUid).uid;
+	}
+
+	@After
+	public void teardown() throws Exception {
+		JdbcTestHelper.getInstance().afterTest();
+	}
+
+	@Test
+	public void testUserWebAppDataLifecycle() throws Exception {
+		String user = "user1";
+		String userUid = PopulateHelper.addUser(user, domainUid);
+		String webAppDataContainerUid = IWebAppDataUids.containerUid(user);
+
+		IContainers containerService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+				.instance(IContainers.class);
+		assertNotNull(containerService.get(webAppDataContainerUid));
+
+		IWebAppData webAppDataService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+				.instance(IWebAppData.class, webAppDataContainerUid);
+		String itemUid = UIDGenerator.uid();
+		webAppDataService.create(itemUid, exampleWebAppData());
+		assertNotNull(webAppDataService.get(itemUid));
+
+		IUser userService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM).instance(IUser.class,
+				domainUid);
+		TaskUtils.wait(ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM), userService.delete(userUid));
+
+		assertNull(webAppDataService.get(itemUid));
+		assertNull(containerService.getIfPresent(webAppDataContainerUid));
+	}
+
+	private WebAppData exampleWebAppData() {
+		WebAppData data = new WebAppData();
+		data.key = "mailapp:expanded_folders";
+		data.value = "['1fa7ea8a59bbec5c', '3db4e71c5b519fad']";
+		return data;
+	}
+
+}
---
+++ parent/webappdata/net.bluemind.webappdata.service/src/net/bluemind/webappdata/service/internal/WebAppDataUserHook.java
@@ -0,0 +1,79 @@
+/* BEGIN LICENSE
+ * Copyright © Blue Mind SAS, 2012-2022
+ *
+ * This file is part of BlueMind. BlueMind is a messaging and collaborative
+ * solution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See LICENSE.txt
+ * END LICENSE
+ */
+package net.bluemind.webappdata.service.internal;
+
+import java.util.Arrays;
+
+import org.slf4j.LoggerFactory;
+
+import net.bluemind.core.api.fault.ServerFault;
+import net.bluemind.core.container.api.ContainerSubscription;
+import net.bluemind.core.container.api.IContainerManagement;
+import net.bluemind.core.container.api.IContainers;
+import net.bluemind.core.container.model.ContainerDescriptor;
+import net.bluemind.core.container.model.ItemValue;
+import net.bluemind.core.context.SecurityContext;
+import net.bluemind.core.rest.BmContext;
+import net.bluemind.core.rest.ServerSideServiceProvider;
+import net.bluemind.user.api.IUserSubscription;
+import net.bluemind.user.api.User;
+import net.bluemind.user.hook.DefaultUserHook;
+import net.bluemind.webappdata.api.IWebAppData;
+import net.bluemind.webappdata.api.IWebAppDataUids;
+
+public class WebAppDataUserHook extends DefaultUserHook {
+
+	@Override
+	public void onUserCreated(BmContext context, String domainUid, ItemValue<User> created) throws ServerFault {
+		String owner = created.uid;
+		String containerUid = IWebAppDataUids.containerUid(owner);
+
+		ContainerDescriptor descriptor = ContainerDescriptor.create(containerUid, containerUid, owner,
+				IWebAppDataUids.TYPE, domainUid, true);
+		ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM).instance(IContainers.class).create(containerUid,
+				descriptor);
+
+		// CHECKME: does user really need to subscribe its webappdata container ?
+		IUserSubscription userSubService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+				.instance(IUserSubscription.class, domainUid);
+		userSubService.subscribe(owner, Arrays.asList(ContainerSubscription.create(containerUid, true)));
+	}
+
+	@Override
+	public void beforeDelete(BmContext context, String domainUid, String uid, User previous) throws ServerFault {
+		String containerUid = IWebAppDataUids.containerUid(uid);
+		IContainers containerService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+				.instance(IContainers.class);
+
+		if (containerService.getIfPresent(containerUid) != null) {
+			IContainerManagement containerManagementService = ServerSideServiceProvider
+					.getProvider(SecurityContext.SYSTEM).instance(IContainerManagement.class);
+			IWebAppData webAppDataService = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+					.instance(IWebAppData.class);
+			try {
+				containerManagementService.getAllItems().forEach(item -> {
+					webAppDataService.delete(item.uid);
+				});
+				containerService.delete(containerUid);
+			} catch (Exception e) {
+				LoggerFactory.getLogger(WebAppDataUserHook.class).warn("Cannot delete container {}", containerUid, e);
+			}
+		}
+	}
+}
