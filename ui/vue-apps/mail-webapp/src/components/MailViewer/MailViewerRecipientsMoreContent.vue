4a5c9ffbe35d8b7b2e2f5d47f21b78b9c1d51fa6
Christophe Fuzier <christophe.fuzier@bluemind.net>
2022-07-13 12:30:30 +0200

    BM-18717 Fix: recipients popover labels color

--- ui/vue-apps/mail-webapp/src/components/MailViewer/MailViewerRecipientsMoreContent.vue
+++ ui/vue-apps/mail-webapp/src/components/MailViewer/MailViewerRecipientsMoreContent.vue
@@ -7,13 +7,13 @@
         </div>
         <div class="overflow-auto pb-2">
             <div class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.from") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.from") }}</div>
                 <div class="custom-col-right">
                     <bm-contact :contact="message.from" no-avatar transparent bold show-address />
                 </div>
             </div>
             <div class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.to") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.to") }}</div>
                 <div class="custom-col-right d-flex flex-column">
                     <bm-contact
                         v-for="(contact, index) in message.to"
@@ -28,7 +28,7 @@
                 </div>
             </div>
             <div v-if="message.cc && message.cc.length" class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.cc") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.cc") }}</div>
                 <div class="custom-col-right d-flex flex-column">
                     <bm-contact
                         v-for="(contact, index) in message.cc"
@@ -43,7 +43,7 @@
                 </div>
             </div>
             <div v-if="message.bcc && message.bcc.length" class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.bcc") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.bcc") }}</div>
                 <div class="custom-col-right d-flex flex-column">
                     <bm-contact
                         v-for="(contact, index) in message.bcc"
@@ -62,11 +62,11 @@
                 <div class="custom-col-right"><hr class="mb-1 mt-3" /></div>
             </div>
             <div class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.date") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.date") }}</div>
                 <div class="fcustom-col-right font-weight-bold">{{ new Date(message.date).toLocaleString() }}</div>
             </div>
             <div class="d-flex">
-                <div class="custom-col-left text-secondary text-right pr-2">{{ $t("common.subject") }}</div>
+                <div class="custom-col-left text-right pr-2">{{ $t("common.subject") }}</div>
                 <div class="custom-col-right font-weight-bold">{{ message.subject }}</div>
             </div>
         </div>
@@ -93,10 +93,13 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~@bluemind/styleguide/css/_variables";
+
 .mail-viewer-recipients-more-content {
     $left-col-width: 4rem;
     .custom-col-left {
         width: $left-col-width;
+        color: $neutral-fg;
     }
     .custom-col-right {
         width: calc(100% - $left-col-width);
