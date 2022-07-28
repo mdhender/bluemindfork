6017d8a68dbc9b99fb9a8ab6a69ca9e7d8c07255
Vincent Vignaud <vincent.vignaud@bluemind.net>
2022-07-28 14:42:44 +0200

    [MailApp] BM-18787 Fix: handle not-loaded message in message viewer

--- ui/vue-apps/mail-webapp/src/components/MailThread/MailMessagePanel.vue
+++ ui/vue-apps/mail-webapp/src/components/MailThread/MailMessagePanel.vue
@@ -1,11 +1,12 @@
 <template>
     <article class="mail-message-panel overflow-x-hidden d-flex" :aria-label="$t('mail.application.region.message')">
-        <mail-message v-if="ACTIVE_MESSAGE" class="flex-grow-1" />
+        <mail-message v-if="ACTIVE_MESSAGE && ACTIVE_MESSAGE.loading === LoadingStatus.LOADED" class="flex-grow-1" />
         <mail-viewer-loading v-else />
     </article>
 </template>
 
 <script>
+import { loadingStatusUtils } from "@bluemind/mail";
 import MailMessage from "./MailMessage";
 import MailViewerLoading from "../MailViewer/MailViewerLoading";
 import { ACTIVE_MESSAGE } from "~/getters";
@@ -14,6 +15,9 @@ import { mapGetters } from "vuex";
 export default {
     name: "MailMessagePanel",
     components: { MailMessage, MailViewerLoading },
+    data() {
+        return { LoadingStatus: loadingStatusUtils.LoadingStatus };
+    },
     computed: {
         ...mapGetters("mail", { ACTIVE_MESSAGE })
     }
