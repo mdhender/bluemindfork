ed83a5272000274d1f52f42c617517b952154673
Mathilde Michau <mathilde.michau@bluemind.net>
2023-01-10 12:10:38 +0100

    Check if message is encrypted when no SW

--- ui/vue-apps/plugins/net.bluemind.plugins.smime/src/components/mail-app/SmimeBodyWrapper.vue
+++ ui/vue-apps/plugins/net.bluemind.plugins.smime/src/components/mail-app/SmimeBodyWrapper.vue
@@ -1,6 +1,6 @@
 <script>
 import { BmButton } from "@bluemind/ui-components";
-import { IS_SW_AVAILABLE } from "../../lib/constants";
+import { IS_SW_AVAILABLE, PKCS7_MIMES } from "../../lib/constants";
 import { isDecrypted, isEncrypted, isSigned, isVerified } from "../../lib/helper";
 import untrustedIllustration from "../../../assets/mail-app-untrusted.png";
 import undecryptedIllustration from "../../../assets/mail-app-undecrypted.png";
@@ -35,13 +35,22 @@ export default {
             return isEncrypted(this.message.headers) && !isDecrypted(this.message.headers);
         }
     },
+    methods: {
+        hasEncryptedPart(partsByCapabilities) {
+            const isEncrypted = ({ mime }) => PKCS7_MIMES.includes(mime);
+            return partsByCapabilities.some(({ parts }) => parts.some(isEncrypted));
+        }
+    },
     render(h) {
         const src = this.untrusted ? untrustedIllustration : undecryptedIllustration;
         const text = this.untrusted
             ? this.$t("common.whats_going_on")
             : this.$t("smime.mailapp.body_wrapper.cant_display");
-
-        if (!IS_SW_AVAILABLE || (!this.forceDisplay && this.untrusted) || this.undecrypted) {
+        if (
+            (!this.forceDisplay && this.untrusted) ||
+            this.undecrypted ||
+            (!IS_SW_AVAILABLE && this.hasEncryptedPart(this.message.inlinePartsByCapabilities))
+        ) {
             const imgDiv = h("div", {}, [h("img", { attrs: { src } })]);
             //  FIXME doc urls: verification failure, decryption failure and missing SW
             const button = h(
