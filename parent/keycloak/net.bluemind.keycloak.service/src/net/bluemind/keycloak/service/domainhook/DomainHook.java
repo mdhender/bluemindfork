0034f05bfd9b9acd9bff33e27efd2350d2870957
David Phan <david.phan@blue-mind.net>
2023-01-12 09:12:22 +0100

    FEATBL-1933 Feat: keycloak create/delete domainhook

---
+++ parent/keycloak/net.bluemind.keycloak.service/src/net/bluemind/keycloak/service/domainhook/DomainHook.java
@@ -0,0 +1,50 @@
+/* BEGIN LICENSE
+  * Copyright © Blue Mind SAS, 2012-2023
+  *
+  * This file is part of BlueMind. BlueMind is a messaging and collaborative
+  * solution.
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of either the GNU Affero General Public License as
+  * published by the Free Software Foundation (version 3 of the License).
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  *
+  * See LICENSE.txt
+  * END LICENSE
+  */
+package net.bluemind.keycloak.service.domainhook;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.bluemind.core.api.fault.ServerFault;
+import net.bluemind.core.container.model.ItemValue;
+import net.bluemind.core.rest.BmContext;
+import net.bluemind.domain.api.Domain;
+import net.bluemind.domain.hook.DomainHookAdapter;
+import net.bluemind.keycloak.api.IKeycloakAdmin;
+import net.bluemind.keycloak.api.IKeycloakUids;
+
+public class DomainHook extends DomainHookAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(DomainHook.class);
+
+	@Override
+	public void onCreated(BmContext context, ItemValue<Domain> domain) throws ServerFault {
+		logger.info("Init Keycloak realm for domain {}", domain.uid);
+		IKeycloakAdmin service = context.provider().instance(IKeycloakAdmin.class);
+		service.createRealm(domain.uid);
+		service.createClient(domain.uid, IKeycloakUids.clientId(domain.uid));
+	}
+
+	@Override
+	public void onDeleted(BmContext context, ItemValue<Domain> domain) throws ServerFault {
+		logger.info("Delete Keycloak realm for domain {}", domain.uid);
+		IKeycloakAdmin service = context.provider().instance(IKeycloakAdmin.class);
+		service.deleteRealm(domain.uid);
+	}
+
+}
