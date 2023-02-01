35418e9230ff6b423bfeae04e7460dc14aa28387
Anthony Prades <anthony.prades@bluemind.net>
2023-02-01 19:37:34 +0100

    BM-19282 Fix: support for parameters with white space from command file

--- parent/cli/net.bluemind.cli.launcher/src/net/bluemind/cli/launcher/CLIEntryPoint.java
+++ parent/cli/net.bluemind.cli.launcher/src/net/bluemind/cli/launcher/CLIEntryPoint.java
@@ -21,6 +21,8 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -31,16 +33,14 @@ import org.fusesource.jansi.AnsiConsole;
 import org.osgi.framework.Version;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import com.google.common.base.Splitter;
-
 public class CLIEntryPoint implements IApplication {
-	
+
 	static {
 		// HOLLOW uses JUL...
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
 		SLF4JBridgeHandler.install();
 	}
-	
+
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		Integer returncode = EXIT_OK;
@@ -53,11 +53,13 @@ public class CLIEntryPoint implements IApplication {
 				Path script = Paths.get(args[0]);
 				CLIManager cm = new CLIManager(version);
 				try (Stream<String> linesStream = Files.lines(script)) {
-					Optional<Integer> returncodes = linesStream.map(this::asArguments).map(cmdargs -> {
-						System.err.println(Ansi.ansi().fgBrightBlue()// NOSONAR
-								.a("Execute from " + script + ": ").reset().a(String.join(" ", cmdargs)));
-						return cm.processArgs(cmdargs);
-					}).filter(ret -> !EXIT_OK.equals(ret)).findAny();
+					Optional<Integer> returncodes = linesStream.map(this::asArguments)
+							.map(optArgs -> optArgs.map(cmdargs -> {
+								System.err.println(Ansi.ansi().fgBrightBlue() // NOSONAR
+										.a("Execute from " + script + ": ").reset().a(String.join(" ", cmdargs)));
+								return cm.processArgs(cmdargs.toArray(new String[0]));
+							}).orElse(EXIT_OK)).filter(ret -> !EXIT_OK.equals(ret)).findAny();
+
 					if (returncodes.isPresent()) {
 						returncode = returncodes.get();
 					}
@@ -75,8 +77,58 @@ public class CLIEntryPoint implements IApplication {
 		return args.length == 1 && new File(args[0]).exists();
 	}
 
-	private String[] asArguments(String line) {
-		return Splitter.on(' ').omitEmptyStrings().splitToList(line).toArray(new String[0]);
+	// Based on:
+	// http://www.java2s.com/example/java-utility-method/string-split-by-quote/quoteawaresplit-string-str-char-delim-74811.html
+	private Optional<List<String>> asArguments(String str) {
+		boolean inQuotes = false;
+		boolean inEscape = false;
+
+		List<String> elements = new ArrayList<>();
+		StringBuilder buffer = new StringBuilder();
+		for (char c : str.toCharArray()) {
+			if (c == ' ' && !inQuotes) {
+				elements.add(buffer.toString());
+				buffer.setLength(0); // clear
+				inEscape = false;
+				continue;
+			}
+
+			if (c == '"') {
+				if (!inEscape) {
+					if (inQuotes) {
+						inQuotes = false;
+					} else {
+						inQuotes = true;
+					}
+					continue;
+				}
+
+				inEscape = false;
+				buffer.append(c);
+				continue;
+			}
+
+			if (c == '\\') {
+				if (!inEscape) {
+					inEscape = true;
+					buffer.append(c);
+					continue;
+				}
+			}
+
+			// all other characters
+			inEscape = false;
+			buffer.append(c);
+		}
+
+		if (inQuotes) {
+			System.err.println(Ansi.ansi().fgBrightBlue().a("Invalid line from: ").reset().a(str));
+			return Optional.empty();
+		}
+
+		elements.add(buffer.toString());
+
+		return Optional.of(elements);
 	}
 
 	@Override
