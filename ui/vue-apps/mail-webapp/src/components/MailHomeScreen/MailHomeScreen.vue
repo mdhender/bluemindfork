6e190d21c99d00d3d179f064b1bd5412751a4f42
Christophe Fuzier <christophe.fuzier@bluemind.net>
2023-02-20 17:07:27 +0100

    FEATWEBML-2338 Feat: migrate multiple selection screen

--- ui/vue-apps/mail-webapp/src/components/MailHomeScreen/MailHomeScreen.vue
+++ ui/vue-apps/mail-webapp/src/components/MailHomeScreen/MailHomeScreen.vue
@@ -30,3 +30,82 @@ export default {
     components: { BmExtension }
 };
 </script>
+
+<style lang="scss">
+@import "~@bluemind/ui-components/src/css/mixins/responsiveness";
+@import "~@bluemind/ui-components/src/css/type";
+@import "~@bluemind/ui-components/src/css/variables";
+
+.mail-home-screen {
+    display: flex;
+    @include until-lg {
+        display: none;
+    }
+
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: max-content;
+    min-height: 100%;
+    width: 100%;
+    padding: $sp-6 0 $sp-5 $scroll-width;
+
+    .starter-text-and-actions {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: $sp-7;
+
+        .starter-text {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+            gap: $sp-7;
+            margin-bottom: $sp-3;
+
+            @extend %regular-high;
+
+            h1 {
+                margin: 0;
+            }
+
+            .description {
+                width: 60%;
+                min-width: base-px-to-rem(240);
+                color: $neutral-fg;
+            }
+        }
+
+        &.compact .starter-text {
+            gap: $sp-5;
+            margin-bottom: 0;
+        }
+
+        .description {
+            width: 60%;
+            min-width: base-px-to-rem(240);
+            min-height: base-px-to-rem(100);
+            display: flex;
+            align-items: center;
+            color: $neutral-fg;
+        }
+    }
+
+    .starter-links {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: $sp-5 $sp-7;
+        margin: $sp-5 0;
+
+        .starter-link {
+            display: flex;
+            gap: $sp-4;
+            .bm-icon {
+                color: $neutral-fg;
+            }
+        }
+    }
+}
+</style>
--- ui/vue-apps/mail-webapp/src/components/MailMultipleSelectionActions.vue
+++ ui/vue-apps/mail-webapp/src/components/MailMultipleSelectionActions.vue
@@ -1,104 +1,75 @@
 <template>
-    <div class="mail-multiple-selection-actions">
-        <bm-alert-area :alerts="alerts" class="w-100" @remove="REMOVE">
+    <div class="mail-multiple-selection-screen">
+        <bm-alert-area :alerts="[]" class="w-100" @remove="REMOVE">
             <template v-slot="context">
                 <component :is="context.alert.renderer" :alert="context.alert" />
             </template>
         </bm-alert-area>
-        <div
-            class="h-100 mt-7 d-flex flex-column text-center align-items-center"
-            :style="'background: url(' + multipleSelectionIllustration + ') no-repeat center top'"
-        >
-            <div class="font-weight-bold mt-7 mb-4">
-                <h1>{{ mainText }}</h1>
-            </div>
-
-            <div
-                v-if="ALL_SELECTED_CONVERSATIONS_ARE_WRITABLE"
-                class="bg-surface pt-2 py-5 px-6 actions-button w-75 mt-7"
-            >
-                <div class="arrow-up" />
-                <bm-button
-                    v-if="showMarkAsReadInMain() || showMarkAsReadInOthers()"
-                    variant="outline"
-                    :title="markAsReadAriaText()"
-                    :aria-label="markAsReadAriaText()"
-                    icon="read"
-                    @click="markAsRead()"
-                    >{{ markAsReadText }}
-                </bm-button>
-                <bm-button
-                    v-if="showMarkAsUnreadInMain() || showMarkAsUnreadInOthers()"
-                    variant="outline"
-                    :title="markAsUnreadAriaText()"
-                    :aria-label="markAsUnreadAriaText()"
-                    icon="unread"
-                    @click="markAsUnread()"
-                >
-                    {{ markAsUnreadText }}
-                </bm-button>
-                <bm-button
-                    v-if="showMarkAsFlaggedInMain || showMarkAsFlaggedInOthers"
-                    class="mark-as-flagged-button"
-                    variant="outline"
-                    :title="markAsFlaggedAriaText()"
-                    :aria-label="markAsFlaggedAriaText()"
-                    icon="flag-fill"
-                    @click="markAsFlagged()"
-                >
-                    {{ markAsFlaggedText }}
-                </bm-button>
-                <bm-button
-                    v-if="showMarkAsUnflaggedInMain || showMarkAsUnflaggedInOthers"
-                    variant="outline"
-                    :title="markAsUnflaggedAriaText()"
-                    :aria-label="markAsUnflaggedAriaText()"
-                    icon="flag-outline"
-                    @click="markAsUnflagged()"
-                >
-                    {{ markAsUnflaggedText }}
-                </bm-button>
-                <bm-button
-                    variant="outline"
-                    :title="removeAriaText()"
-                    :aria-label="removeAriaText()"
-                    icon="trash"
-                    @click.exact="moveToTrash"
-                    @click.shift.exact="remove"
-                    >{{ removeText }}
-                </bm-button>
-                <bm-button variant="outline" :title="moveAriaText()" icon="folder" @click.exact="openMoveFolderModal"
-                    >{{ moveText }}
+        <section class="mail-home-screen" aria-labelledby="text-1">
+            <div class="starter-text-and-actions">
+                <div class="starter-text">
+                    <h1 id="text-1">{{ mainText }}</h1>
+                </div>
+                <bm-button variant="text" size="sm" icon="cross" @click="cancelSelection">
+                    {{ $t("common.cancel.selection") }}
                 </bm-button>
             </div>
-
-            <bm-button variant="text" class="my-6" @click="removeSelection">
-                {{ $t("common.cancel.selection") }}
-            </bm-button>
-
-            <hr v-if="!ALL_CONVERSATIONS_ARE_SELECTED" class="w-75 border-neutral" />
-
-            <div v-if="!ALL_CONVERSATIONS_ARE_SELECTED" class="mt-3">
-                <div v-if="!CONVERSATION_LIST_IS_SEARCH_MODE" class="d-inline-flex px-3 align-items-center">
-                    <div>{{ $t("mail.message.select.all.folder") }}</div>
-                    <mail-folder-icon
-                        :mailbox="mailboxes[currentFolder.mailboxRef.key]"
-                        :folder="currentFolder"
-                        class="font-weight-bold ml-3"
+            <div class="illustration-and-actions">
+                <div class="actions">
+                    <bm-button
+                        variant="text"
+                        :title="markAsUnreadAriaText()"
+                        :aria-label="markAsUnreadAriaText()"
+                        icon="unread"
+                        @click="markAsUnread()"
                     >
-                        {{ currentFolder.path }}
-                    </mail-folder-icon>
+                        {{ markAsUnreadText }}
+                    </bm-button>
+                    <bm-button
+                        variant="text"
+                        :title="markAsReadAriaText()"
+                        :aria-label="markAsReadAriaText()"
+                        icon="read"
+                        @click="markAsRead()"
+                    >
+                        {{ markAsReadText }}
+                    </bm-button>
+                    <bm-button
+                        class="mark-as-flagged-button"
+                        variant="text"
+                        :title="markAsFlaggedAriaText()"
+                        :aria-label="markAsFlaggedAriaText()"
+                        icon="flag-fill"
+                        @click="markAsFlagged()"
+                    >
+                        {{ markAsFlaggedText }}
+                    </bm-button>
+                    <bm-button
+                        variant="text"
+                        :title="markAsUnflaggedAriaText()"
+                        :aria-label="markAsUnflaggedAriaText()"
+                        icon="flag-outline"
+                        @click="markAsUnflagged()"
+                    >
+                        {{ markAsUnflaggedText }}
+                    </bm-button>
+                    <bm-button variant="text" :title="moveAriaText()" icon="folder" @click.exact="openMoveFolderModal">
+                        {{ moveText }}
+                    </bm-button>
+                    <bm-button
+                        variant="text"
+                        :title="removeAriaText()"
+                        :aria-label="removeAriaText()"
+                        icon="trash"
+                        @click.exact="moveToTrash"
+                        @click.shift.exact="remove"
+                    >
+                        {{ removeText }}
+                    </bm-button>
                 </div>
-                <div v-else class="d-inline-flex px-3 align-items-center">
-                    <div>{{ $t("mail.message.select.all.search") }}</div>
-                    <bm-icon icon="search" class="mx-3" />
-                    <span class="font-weight-bold">"{{ conversationList.search.pattern }}"</span>
-                </div>
-                <bm-button @click="SET_SELECTION(CONVERSATION_LIST_ALL_KEYS)">
-                    {{ $t("common.select.all") }}
-                </bm-button>
+                <bm-illustration :value="illustration" size="lg" over-background />
             </div>
-        </div>
+        </section>
         <choose-folder-modal
             ref="move-modal"
             :ok-title="moveText"
@@ -113,75 +84,71 @@
 </template>
 
 <script>
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
+
 import { CLEAR, INFO, REMOVE } from "@bluemind/alert.store";
-import { BmAlertArea, BmButton, BmLabelIcon, BmIcon } from "@bluemind/ui-components";
-import { mapActions, mapState, mapGetters, mapMutations } from "vuex";
+import { CONVERSATIONS_ACTIVATED, MAILBOX_INBOX, MAILBOX_JUNK, SELECTION_KEYS } from "~/getters";
+import { UNSELECT_ALL_CONVERSATIONS } from "~/mutations";
+
 import { folderUtils } from "@bluemind/mail";
-import MailFolderIcon from "./MailFolderIcon";
-import multipleSelectionIllustration from "../../assets/multiple-selection.png";
+
+import { BmAlertArea, BmButton, BmIllustration } from "@bluemind/ui-components";
 
 import { ActionTextMixin, FlagMixin, MoveMixin, RemoveMixin, SelectionMixin } from "~/mixins";
-
-import {
-    ALL_CONVERSATIONS_ARE_SELECTED,
-    ALL_SELECTED_CONVERSATIONS_ARE_WRITABLE,
-    CONVERSATIONS_ACTIVATED,
-    CONVERSATION_LIST_ALL_KEYS,
-    CONVERSATION_LIST_IS_SEARCH_MODE,
-    MAILBOX_INBOX,
-    MAILBOX_JUNK,
-    SELECTION_KEYS
-} from "~/getters";
-
-import { SET_SELECTION, UNSELECT_ALL_CONVERSATIONS } from "~/mutations";
 import ChooseFolderModal from "./ChooseFolderModal";
 
 const { DEFAULT_FOLDERS } = folderUtils;
 
 export default {
-    name: "MailMultipleSelectionActions",
+    name: "MailMultipleSelectionScreen",
     components: {
         BmAlertArea,
         BmButton,
-        BmIcon,
-        BmLabelIcon,
-        MailFolderIcon,
+        BmIllustration,
         ChooseFolderModal
     },
     mixins: [ActionTextMixin, FlagMixin, MoveMixin, RemoveMixin, SelectionMixin],
     data() {
         return {
             isReadOnlyAlertDismissed: false,
-            multipleSelectionIllustration,
             defaultFolders: []
         };
     },
     computed: {
-        ...mapState("mail", ["activeFolder", "folders", "mailboxes", "conversationList"]),
-        ...mapState("mail", { messages: ({ conversations }) => conversations.messages }),
+        ...mapState("mail", ["activeFolder", "folders", "mailboxes"]),
         ...mapGetters("mail", {
-            ALL_CONVERSATIONS_ARE_SELECTED,
-            ALL_SELECTED_CONVERSATIONS_ARE_WRITABLE,
             CONVERSATIONS_ACTIVATED,
-            CONVERSATION_LIST_ALL_KEYS,
-            CONVERSATION_LIST_IS_SEARCH_MODE,
+            MAILBOX_INBOX,
+            MAILBOX_JUNK,
             SELECTION_KEYS
         }),
         ...mapState({ alerts: state => state.alert.filter(({ area }) => area === "right-panel") }),
-        currentFolder() {
-            return this.folders[this.activeFolder];
+
+        selectionSize() {
+            return this.SELECTION_KEYS.length;
+        },
+        illustration() {
+            if (2 <= this.selectionSize && this.selectionSize <= 4) {
+                return `stack-${this.selectionSize}`;
+            }
+            if (this.selectionSize <= 100) {
+                return "stack-small";
+            }
+            return "stack-large";
         },
         mainText() {
-            const count = this.SELECTION_KEYS.length;
             return this.CONVERSATIONS_ACTIVATED
-                ? this.$t("mail.conversations.selected", { count })
-                : this.$t("mail.message.selected", { count });
+                ? this.$t("mail.conversations.selected", { count: this.selectionSize })
+                : this.$t("mail.message.selected", { count: this.selectionSize });
         },
         readOnlyAlert() {
             return {
                 alert: { name: "mail.READ_ONLY_FOLDER", uid: "READ_ONLY_FOLDER" },
                 options: { area: "right-panel", renderer: "DefaultAlert" }
             };
+        },
+        currentFolder() {
+            return this.folders[this.activeFolder];
         },
         mailbox() {
             return this.mailboxes[this.currentFolder.mailboxRef.key];
@@ -208,9 +175,9 @@ export default {
         this.CLEAR("right-panel");
     },
     methods: {
-        ...mapMutations("mail", { SET_SELECTION, UNSELECT_ALL_CONVERSATIONS }),
+        ...mapMutations("mail", { UNSELECT_ALL_CONVERSATIONS }),
         ...mapActions("alert", { REMOVE, CLEAR, INFO }),
-        removeSelection() {
+        cancelSelection() {
             this.UNSELECT_ALL_CONVERSATIONS();
         },
         isExcluded(folder) {
@@ -228,53 +195,62 @@ export default {
         move(folder) {
             this.MOVE_CONVERSATIONS({ conversations: this.selected, folder });
         }
-    }
+    },
+
+    priority: 1024
 };
 </script>
 
 <style lang="scss">
-@use "sass:math";
-@import "@bluemind/ui-components/src/css/mixins/_responsiveness.scss";
 @import "~@bluemind/ui-components/src/css/variables";
 
-$arrow-width: 3rem;
-$arrow-height: math.div($arrow-width, 2);
+.mail-multiple-selection-screen {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 
-.mail-multiple-selection-actions {
-    @include until-lg {
-        display: none;
-    }
+    .mail-home-screen {
+        min-height: unset;
+        flex: 1 0 max-content;
+        padding-bottom: $sp-7;
+        gap: $sp-7;
 
-    h1 {
-        color: $neutral-fg-hi1;
-        font-size: 2rem;
-    }
-    hr {
-        height: 1px;
-    }
-    .actions-button {
-        .btn {
-            margin: $sp-4 $sp-3;
+        .starter-text-and-actions {
+            gap: $sp-6;
+
+            .starter-text {
+                margin-bottom: 0;
+            }
         }
-        .mark-as-flagged-button .fa-flag-fill {
-            color: $warning-fg;
-        }
-        .arrow-up {
-            position: relative;
-            display: block;
-            width: $arrow-width;
-            height: $arrow-height;
-            bottom: $arrow-height + $sp-2;
-            left: calc(50% - #{$arrow-width});
 
-            &::before {
-                position: absolute;
-                content: "";
-                border-color: transparent;
-                border-style: solid;
-                bottom: 0;
-                border-width: 0 calc($arrow-width / 2) $arrow-height;
-                border-bottom-color: $surface;
+        .illustration-and-actions {
+            display: flex;
+            gap: $sp-7;
+            margin: 0 auto;
+
+            .actions {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                gap: $sp-2;
+                padding-top: base-px-to-rem(44);
+                padding-left: $sp-6;
+
+                .mark-as-flagged-button .fa-flag-fill {
+                    color: $warning-fg;
+                }
+            }
+
+            .bm-illustration {
+                position: relative;
+                width: 334px;
+                height: 310px;
+
+                & > svg {
+                    position: absolute;
+                    left: -96px;
+                    top: -62px;
+                }
             }
         }
     }
