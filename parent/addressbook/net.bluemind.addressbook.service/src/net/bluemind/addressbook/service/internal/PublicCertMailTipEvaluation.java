85d4720f37f6983f17e2d5a4d1ff37e6462a09cb
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2023-03-14 17:25:45 +0100

    Chore: fix mailtips tests

--- parent/addressbook/net.bluemind.addressbook.service/src/net/bluemind/addressbook/service/internal/PublicCertMailTipEvaluation.java
+++ parent/addressbook/net.bluemind.addressbook.service/src/net/bluemind/addressbook/service/internal/PublicCertMailTipEvaluation.java
@@ -18,6 +18,7 @@
  */
 package net.bluemind.addressbook.service.internal;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -26,6 +27,7 @@ import net.bluemind.addressbook.api.IAddressBooks;
 import net.bluemind.addressbook.api.VCardQuery;
 import net.bluemind.core.context.SecurityContext;
 import net.bluemind.core.rest.ServerSideServiceProvider;
+import net.bluemind.directory.api.DirEntry;
 import net.bluemind.directory.api.IDirectory;
 import net.bluemind.mailflow.common.api.Recipient;
 import net.bluemind.mailmessage.api.IMailTipEvaluation;
@@ -40,8 +42,12 @@ public class PublicCertMailTipEvaluation implements IMailTipEvaluation {
 
 	@Override
 	public List<EvaluationResult> evaluate(String domainUid, MessageContext messageContext) {
-		var senderUid = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
-				.instance(IDirectory.class, domainUid).getByEmail(messageContext.fromIdentity.sender).entryUid;
+		DirEntry sender = ServerSideServiceProvider.getProvider(SecurityContext.SYSTEM)
+				.instance(IDirectory.class, domainUid).getByEmail(messageContext.fromIdentity.sender);
+		if (sender == null) {
+			return Collections.emptyList();
+		}
+		String senderUid = sender.entryUid;
 		try (Sudo sudo = new Sudo(senderUid, domainUid)) {
 			var books = ServerSideServiceProvider.getProvider(sudo.context).instance(IAddressBooks.class);
 			return messageContext.recipients.stream().map(r -> process(books, r)).filter(Objects::nonNull)
