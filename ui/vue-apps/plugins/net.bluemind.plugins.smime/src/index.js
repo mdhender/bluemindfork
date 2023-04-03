75245a07af604043f4b0ef8b0b874b71a3760bcb
kladier <kevin.ladier@bluemind.net>
2023-04-03 17:24:47 +0200

    FEATWEBML-2486 [S/MIME] Feat: register extensions according to S/MIME role

--- ui/vue-apps/plugins/net.bluemind.plugins.smime/src/index.js
+++ ui/vue-apps/plugins/net.bluemind.plugins.smime/src/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import { extensions } from "@bluemind/extensions";
 import i18n, { TranslationRegistry } from "@bluemind/i18n";
+import BmRoles from "@bluemind/roles";
 import store from "@bluemind/store";
 import AddCertificateAlert from "./components/mail-app/alerts/AddCertificateAlert";
 import CertificateViewer from "./components/mail-app/CertificateViewer";
@@ -27,99 +28,23 @@ import { SMIME_AVAILABLE } from "./store/getterTypes";
 import GetMailTipsHandler from "./commands/GetMailTipsHandler";
 
 TranslationRegistry.register(SmimeL10N);
-
 store.registerModule(["mail", "smime"], SmimeStore);
-store.dispatch("mail/" + CHECK_IF_ASSOCIATED);
 
-Vue.component("AddCertificateAlert", AddCertificateAlert);
-Vue.component("CertificateViewer", CertificateViewer);
-Vue.component("ContactWithCertificate", ContactWithCertificate);
-Vue.component("DecryptErrorAlert", DecryptErrorAlert);
-Vue.component("DecryptErrorTrigger", DecryptErrorTrigger);
-Vue.component("EncryptAndSignButton", EncryptAndSignButton);
-Vue.component("EncryptErrorAlert", EncryptErrorAlert);
-Vue.component("InvalidIdentityAlert", InvalidIdentityAlert);
-Vue.component("LockIcon", LockIcon);
-Vue.component("CertificateFileItem", CertificateFileItem);
-Vue.component("PrefSMime", PrefSMime);
-Vue.component("SignErrorAlert", SignErrorAlert);
+const userRoles = window.bmcSessionInfos.roles.split(",");
+if (userRoles.includes(BmRoles.CAN_USE_SMIME)) {
+    store.dispatch("mail/" + CHECK_IF_ASSOCIATED);
+}
+
 Vue.component("SMimeBodyWrapper", SMimeBodyWrapper);
-Vue.component("TrustedSender", TrustedSender);
-Vue.component("UntrustedSenderAlert", UntrustedSenderAlert);
-Vue.component("UntrustedSenderTrigger", UntrustedSenderTrigger);
-Vue.component("AddCertificateButton", AddCertificateButton);
-
-extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
-    component: {
-        name: "LockIcon",
-        path: "list.conversation.icon",
-        priority: 10
-    }
-});
-
-extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
-    component: {
-        path: "viewer.sender.suffix",
-        name: "TrustedSender"
-    }
-});
-
-extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
-    component: {
-        path: "viewer.header",
-        name: "UntrustedSenderTrigger"
-    }
-});
-
-extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
-    component: {
-        path: "viewer.header",
-        name: "DecryptErrorTrigger"
-    }
-});
-
 extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
     component: {
         path: "viewer.body",
         name: "SMimeBodyWrapper"
     }
 });
-extensions.register("webapp", "net.bluemind.plugins.smime", {
-    command: {
-        name: "get-mail-tips",
-        fn: GetMailTipsHandler
-    }
-});
 
-extensions.register("webapp.preferences", "net.bluemind.plugins.smime", {
-    section: {
-        id: "mail",
-        categories: [
-            {
-                id: "security",
-                name: i18n.t("common.security"),
-                icon: "key",
-                groups: prefSmimeGroups()
-            }
-        ]
-    }
-});
-
+Vue.component("CertificateFileItem", CertificateFileItem);
 extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
-    component: {
-        name: "EncryptAndSignButton",
-        path: "composer.footer.toolbar"
-    }
-});
-
-extensions.register("webapp", "net.bluemind.plugins.smime", {
-    component: {
-        name: "ContactWithCertificate",
-        path: "contact.chip.mail.composer.recipients"
-    }
-});
-
-extensions.register("webapp.mail", "file-item", {
     component: {
         name: "CertificateFileItem",
         path: "message.file",
@@ -127,18 +52,107 @@ extensions.register("webapp.mail", "file-item", {
     }
 });
 
+Vue.component("AddCertificateAlert", AddCertificateAlert);
+Vue.component("CertificateViewer", CertificateViewer);
 extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
     component: {
         name: "CertificateViewer",
         path: "file.preview",
-        priority: 10
+        priority: 10,
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("LockIcon", LockIcon);
+extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
+    component: {
+        name: "LockIcon",
+        path: "list.conversation.icon",
+        priority: 10,
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("TrustedSender", TrustedSender);
+extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
+    component: {
+        name: "TrustedSender",
+        path: "viewer.sender.suffix",
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("UntrustedSenderAlert", UntrustedSenderAlert);
+Vue.component("UntrustedSenderTrigger", UntrustedSenderTrigger);
+extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
+    component: {
+        path: "viewer.header",
+        name: "UntrustedSenderTrigger",
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("DecryptErrorAlert", DecryptErrorAlert);
+Vue.component("DecryptErrorTrigger", DecryptErrorTrigger);
+extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
+    component: {
+        path: "viewer.header",
+        name: "DecryptErrorTrigger",
+        role: BmRoles.CAN_USE_SMIME
     }
 });
 
 extensions.register("webapp", "net.bluemind.plugins.smime", {
+    command: {
+        name: "get-mail-tips",
+        fn: GetMailTipsHandler,
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("InvalidIdentityAlert", InvalidIdentityAlert);
+Vue.component("SignErrorAlert", SignErrorAlert);
+Vue.component("EncryptErrorAlert", EncryptErrorAlert);
+Vue.component("EncryptAndSignButton", EncryptAndSignButton);
+extensions.register("webapp.mail", "net.bluemind.plugins.smime", {
+    component: {
+        name: "EncryptAndSignButton",
+        path: "composer.footer.toolbar",
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("ContactWithCertificate", ContactWithCertificate);
+extensions.register("webapp", "net.bluemind.plugins.smime", {
+    component: {
+        name: "ContactWithCertificate",
+        path: "contact.chip.mail.composer.recipients",
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("AddCertificateButton", AddCertificateButton);
+extensions.register("webapp", "net.bluemind.plugins.smime", {
     component: {
         name: "AddCertificateButton",
-        path: "file.actions"
+        path: "file.actions",
+        role: BmRoles.CAN_USE_SMIME
+    }
+});
+
+Vue.component("PrefSMime", PrefSMime);
+extensions.register("webapp.preferences", "net.bluemind.plugins.smime", {
+    section: {
+        id: "mail",
+        categories: [
+            {
+                id: "security",
+                visible: { name: "RoleCondition", args: [BmRoles.CAN_USE_SMIME] },
+                name: i18n.t("common.security"),
+                icon: "key",
+                groups: prefSmimeGroups()
+            }
+        ]
     }
 });
 
