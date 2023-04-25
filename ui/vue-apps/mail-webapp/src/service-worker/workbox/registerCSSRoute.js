e0b564dfa970f9d473b7b7a6d5cc83a2d368bf7b
Mehdi Rande <mehdi.rande@bluemind.net>
2023-04-25 15:50:25 +0200

    BM-19558 Imp: prevent css invading from old bluemind version

--- ui/vue-apps/mail-webapp/src/service-worker/workbox/registerCSSRoute.js
+++ ui/vue-apps/mail-webapp/src/service-worker/workbox/registerCSSRoute.js
@@ -1,12 +1,12 @@
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { NetworkFirst } from "workbox-strategies";
 
 export default function () {
     registerRoute(
         // Cache style resources, i.e. CSS files.
         ({ request }) => request.destination === "style",
         // Use cache but update in the background.
-        new StaleWhileRevalidate({
+        new NetworkFirst({
             // Use a custom cache name.
             cacheName: "css-cache"
         })
