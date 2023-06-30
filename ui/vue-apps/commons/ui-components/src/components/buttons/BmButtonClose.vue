23c3cb37d813ba3055f3973478768acf8d9913aa
Christophe Fuzier <christophe.fuzier@bluemind.net>
2023-06-30 14:51:59 +0200

    FEATWEBML-2563 Feat: reimplement BmButtonClose to stick to mockups

--- ui/vue-apps/commons/ui-components/src/components/buttons/BmButtonClose.vue
+++ ui/vue-apps/commons/ui-components/src/components/buttons/BmButtonClose.vue
@@ -1,13 +1,16 @@
 <template>
-    <b-button-close class="bm-button-close" :disabled="disabled" :class="`btn-close-${size}`" v-on="$listeners" />
+    <b-button v-bind="[$props]" class="bm-button-close" v-on="$listeners">
+        <bm-icon icon="cross-thin" />
+    </b-button>
 </template>
 
 <script>
-import { BButtonClose } from "bootstrap-vue";
+import { BButton } from "bootstrap-vue";
+import BmIcon from "../BmIcon";
 
 export default {
     name: "BmButtonClose",
-    components: { BButtonClose },
+    components: { BButton, BmIcon },
     props: {
         size: {
             type: String,
@@ -23,3 +26,31 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+@import "../../css/utils/variables";
+@import "../../css/utils/buttons";
+
+.bm-button-close {
+    flex: none;
+
+    @include bm-icon-button-compact-variant("neutral");
+
+    @each $name, $value in $btn-close-sizes {
+        &.btn-#{$name} {
+            height: $value;
+            width: $value;
+        }
+    }
+}
+
+@include set-button-icon-sizes(
+    ".bm-button-close",
+    (
+        "xs": "xs",
+        "sm": "xs",
+        "md": "md",
+        "lg": "lg"
+    )
+);
+</style>
