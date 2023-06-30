c515bc5db5829ab878e48139ac9e0dc0629526dc
Christophe Fuzier <christophe.fuzier@bluemind.net>
2023-06-30 11:46:11 +0200

    FEATWEBML-2563 Feat: implement all navbar-related components

---
+++ ui/vue-apps/commons/ui-components/src/components/navbar/BmNavbarBack.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bm-navbar-back">
+        <bm-icon-button variant="compact-on-fill-primary" size="lg" icon="arrow-back" class="h-100" v-on="$listeners" />
+        <div class="separator" />
+    </div>
+</template>
+
+<script>
+import BmIconButton from "../buttons/BmIconButton";
+
+export default {
+    name: "BmNavbarBack",
+    components: { BmIconButton }
+};
+</script>
+
+<style lang="scss">
+@import "@bluemind/ui-components/src/css/utils/variables";
+
+.bm-navbar-back {
+    flex: none;
+    display: flex;
+    align-items: stretch;
+    height: base-px-to-rem(40);
+    .separator {
+        width: $input-border-width;
+        border-right: $input-border-width solid $fill-primary-fg-lo2;
+    }
+}
+</style>
---
+++ ui/vue-apps/commons/ui-components/src/components/navbar/BmNavbarTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bold pl-5 flex-fill text-truncate">
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BmNavbarTitle",
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
