f78c80d412134b9b3291d15c99e9c239324110b2
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2023-08-07 11:44:05 +0200

    [clone] Fix: update global.virt on restore to set openid properties, preserve openid_client_secret if one is set.

--- parent/core/net.bluemind.core.backup.continuous.clone/src/net/bluemind/core/backup/continuous/restore/orphans/RestoreDomains.java
+++ parent/core/net.bluemind.core.backup.continuous.clone/src/net/bluemind/core/backup/continuous/restore/orphans/RestoreDomains.java
@@ -22,12 +22,14 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import net.bluemind.core.api.auth.AuthDomainProperties;
 import net.bluemind.core.backup.continuous.DataElement;
 import net.bluemind.core.backup.continuous.restore.orphans.RestoreTopology.PromotingServer;
 import net.bluemind.core.container.model.ItemValue;
@@ -38,8 +40,11 @@ import net.bluemind.core.utils.JsonUtils.ValueReader;
 import net.bluemind.domain.api.Domain;
 import net.bluemind.domain.api.DomainSettings;
 import net.bluemind.domain.api.IDomainSettings;
+import net.bluemind.domain.api.IDomains;
 import net.bluemind.domain.api.IInCoreDomains;
+import net.bluemind.keycloak.api.IKeycloakAdmin;
 import net.bluemind.server.api.IServer;
+import net.bluemind.server.api.TagDescriptor;
 
 public class RestoreDomains {
 
@@ -98,16 +103,22 @@ public class RestoreDomains {
 			String payload) {
 		ItemValue<Domain> domain = domainReader.read(payload);
 		if (domain.uid.equals("global.virt")) {
+			// update it for keycloak properties
+			tweakKeycloakProps(target.instance(IDomains.class).get("global.virt"), domain);
+			domainApi.restore(domain, false);
+			target.instance(IKeycloakAdmin.class).initForDomain("global.virt");
 			return;
 		}
 		ItemValue<Domain> known = domainApi.get(domain.uid);
 		if (known != null) {
 			logger.info("UPDATE DOMAIN {}", domain);
 			domainApi.setAliases(domain.uid, domain.value.aliases);
+			tweakKeycloakProps(known, domain);
 			domainApi.restore(domain, false);
 		} else {
 			logger.info("CREATE DOMAIN {}", domain);
 			monitor.log("CREATE DOMAIN {}", domain);
+			tweakKeycloakProps(null, domain);
 			domainApi.restore(domain, true);
 			for (PromotingServer iv : servers) {
 				for (String tag : iv.clone.value.tags) {
@@ -117,6 +128,22 @@ public class RestoreDomains {
 			}
 		}
 		domainsToHandle.put(domain.uid, domain);
+	}
+
+	private void tweakKeycloakProps(ItemValue<Domain> prev, ItemValue<Domain> domain) {
+		if (domain.value.properties != null) {
+			String oidSec = AuthDomainProperties.OPENID_CLIENT_SECRET.name();
+			if (prev != null && prev.value.properties != null && prev.value.properties.containsKey(oidSec)) {
+				domain.value.properties.put(oidSec, prev.value.properties.get(oidSec));
+			}
+			servers.stream().filter(pv -> pv.clone.value.tags.contains(TagDescriptor.bm_keycloak.getTag()))
+					.forEach(pv -> {
+						for (Entry<String, String> kv : domain.value.properties.entrySet()) {
+							String fresh = kv.getValue().replace(pv.leader.value.address(), pv.clone.value.address());
+							kv.setValue(fresh);
+						}
+					});
+		}
 	}
 
 }
