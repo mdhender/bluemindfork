e1d6b61bf4ff35c35a2759b8541c265c19f47f03
Laurent Coustet <laurent.coustet@bluemind.net>
2023-09-28 10:20:38 +0200

    fix: net.bluemind.config now watches /etc/bm/bm-core.tok for auto-refresh if changed

--- parent/core/net.bluemind.config/src/net/bluemind/config/Activator.java
+++ parent/core/net.bluemind.config/src/net/bluemind/config/Activator.java
@@ -1,5 +1,11 @@
 package net.bluemind.config;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchService;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -15,10 +21,22 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		InstallationId.init();
+
+		try {
+			Path etcbm = Token.TOKEN_PATH.getParent();
+			WatchService watchService = FileSystems.getDefault().newWatchService(); // NOSONAR: handled in
+																					// Token.stopWatcher
+			etcbm.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY,
+					StandardWatchEventKinds.ENTRY_DELETE);
+			Token.startWatcher(watchService);
+		} catch (IOException ignored) {
+			// ignored
+		}
 	}
 
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
+		Token.stopWatcher();
 		Activator.context = null;
 	}
 
--- parent/core/net.bluemind.config/src/net/bluemind/config/Token.java
+++ parent/core/net.bluemind.config/src/net/bluemind/config/Token.java
@@ -22,6 +22,11 @@ package net.bluemind.config;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,9 +38,26 @@ public final class Token {
 	private static final Logger logger = LoggerFactory.getLogger(Token.class);
 	private static final String NO_TOKEN = "NO_TOKEN";
 	private static String admin0Token;
+	public static final Path TOKEN_PATH = Paths.get("/etc/bm/bm-core.tok");
+
+	private static WatchService watchService = null;
+
+	public static void startWatcher(WatchService watchService) {
+		Token.watchService = watchService;
+	}
+
+	public static void stopWatcher() {
+		if (watchService != null) {
+			try {
+				watchService.close();
+			} catch (IOException ignored) {
+				// ignored
+			}
+		}
+	}
 
 	private static final String loadToken() {
-		File tokenFile = new File("/etc/bm/bm-core.tok");
+		File tokenFile = TOKEN_PATH.toFile();
 		if (tokenFile.exists()) {
 			try {
 				return Files.asCharSource(tokenFile, StandardCharsets.UTF_8).read().trim().replace("\r\n", "");
@@ -55,6 +77,18 @@ public final class Token {
 	public static final String admin0() {
 		if (null == admin0Token || admin0Token.equals(NO_TOKEN)) {
 			admin0Token = loadToken();
+		}
+		if (watchService != null) {
+			WatchKey key;
+			while ((key = watchService.poll()) != null) {
+				for (WatchEvent<?> event : key.pollEvents()) {
+					if (event.context().equals(TOKEN_PATH.getFileName().toString())) {
+						logger.info("token {} changed: reloading", TOKEN_PATH);
+						forceReload();
+					}
+				}
+				key.reset();
+			}
 		}
 		return admin0Token;
 	}
