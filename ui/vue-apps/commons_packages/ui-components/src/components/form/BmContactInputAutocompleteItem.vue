c76dd918f571d295b1c175e14b4b7c108b02ca49
Mathilde Michau <mathilde.michau@bluemind.net>
2023-11-03 18:03:12 +0100

    BM-19340 Fix: Remove delete button for groups without addresses in contact suggestions

--- ui/vue-apps/commons_packages/ui-components/src/components/form/BmContactInputAutocompleteItem.vue
+++ ui/vue-apps/commons_packages/ui-components/src/components/form/BmContactInputAutocompleteItem.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="bm-contact-input-autocomplete-item align-items-center d-flex h-100">
-        <div class="d-flex align-items-center flex-fill py-4 pl-4 pr-2">
+        <div
+            class="d-flex align-items-center flex-fill py-4 pl-4 pr-2"
+            :class="{ 'non-deletable': isGroupWithoutAddress }"
+        >
             <div>
                 <bm-avatar
                     size="sm"
                     :alt="dn"
                     :urn="contact.urn"
-                    :icon="contact.kind === 'group' ? 'group' : undefined"
-                    :color="contact.kind === 'group' ? groupColor : undefined"
+                    :icon="isGroup ? 'group' : undefined"
+                    :color="isGroup ? groupColor : undefined"
                 />
             </div>
-            <div class="flew-grow-1 px-5">
+            <div class="px-5">
                 <!-- eslint-disable-next-line vue/no-v-html -->
                 <div class="contact-dn text-truncate" v-html="boldIt(dn)" />
                 <!-- eslint-disable-next-line vue/no-v-html -->
                 <div class="text-muted text-truncate caption" v-html="address" />
             </div>
-            <div class="ml-auto"><bm-icon :icon="contact.kind === 'group' ? 'group' : 'user'" /></div>
+            <div class="ml-auto"><bm-icon :icon="isGroup ? 'group' : 'user'" /></div>
         </div>
-        <div class="h-100 p-0 delete-autocomplete">
+        <div v-if="!isGroupWithoutAddress" class="h-100 p-0 delete-autocomplete">
             <bm-button-close
                 class="h-100 w-100"
                 size="sm"
@@ -30,6 +33,7 @@
 </template>
 
 <script>
+import { VCard } from "@bluemind/addressbook.api";
 import BmAvatar from "../BmAvatar";
 import BmButtonClose from "../buttons/BmButtonClose";
 import BmIcon from "../BmIcon";
@@ -58,6 +62,12 @@ export default {
             return (
                 this.boldIt(this.contact.address) || this.$tc("styleguide.contact-input.members", count || 1, { count })
             );
+        },
+        isGroup() {
+            return this.contact.kind === VCard.Kind.group;
+        },
+        isGroupWithoutAddress() {
+            return this.isGroup && !this.contact.address;
         }
     },
     methods: {
@@ -102,6 +112,7 @@ export default {
 <style lang="scss">
 @import "../../css/utils/variables.scss";
 
+$delete-btn-width: 1.25rem;
 .bm-contact-input-autocomplete-item {
     .contact-dn {
         color: $neutral-fg-hi1;
@@ -109,8 +120,11 @@ export default {
     }
     .delete-autocomplete {
         visibility: hidden;
-        width: 1.25rem;
+        width: $delete-btn-width;
         border-left: 1px solid $surface;
+    }
+    .non-deletable {
+        margin-right: $delete-btn-width;
     }
     &:hover .delete-autocomplete {
         visibility: visible;
