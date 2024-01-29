017403acde9049dbcd01eaf0bf753fd1a5d7f744
maxime.abehserra <maxime.abehserra@bluemind.net>
2024-01-29 11:35:26 +0100

    [MailApp] BM-19804 Fix: Settings Store - Empty line in font-value selector  - handle empty font value when extra-font was removed by administrator  - handle string with only space by using triming

--- ui/vue-apps/root-webapp/src/settingsStore.js
+++ ui/vue-apps/root-webapp/src/settingsStore.js
@@ -37,14 +37,16 @@ const getters = {
         }
     },
     EXTRA_FONT_FAMILIES: state => {
-        return state.domain_composer_font_stack?.split(";").map(fontValue => {
-            const fontName = fontValue.split(",")[0];
-            return {
-                id: fontName.toLowerCase(),
-                text: fontName,
-                value: fontValue.replaceAll(",", ", ")
-            };
-        });
+        return state.domain_composer_font_stack?.split(";").reduce((fontValues, currentFont) => {
+            const fontName = currentFont.split(",")[0];
+            return fontName.trim()
+                ? fontValues.concat({
+                      id: fontName.toLowerCase(),
+                      text: fontName,
+                      value: currentFont.replaceAll(",", ", ")
+                  })
+                : fontValues;
+        }, []);
     }
 };
 
