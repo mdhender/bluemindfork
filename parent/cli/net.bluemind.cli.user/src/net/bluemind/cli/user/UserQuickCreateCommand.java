df8f428faa8bf0c2e0128fe341c2503be19f05ea
Laurent Coustet <laurent.coustet@bluemind.net>
2024-01-31 16:10:42 +0100

    [cli] feat: bm-cli user quickcreate --count=5000 --pass=password --workers=8 user{}@bm.infra

--- parent/cli/net.bluemind.cli.user/src/net/bluemind/cli/user/UserQuickCreateCommand.java
+++ parent/cli/net.bluemind.cli.user/src/net/bluemind/cli/user/UserQuickCreateCommand.java
@@ -23,7 +23,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.helpers.MessageFormatter;
 
 import io.netty.util.internal.ThreadLocalRandom;
 import net.bluemind.addressbook.api.VCard;
@@ -69,7 +73,7 @@ public class UserQuickCreateCommand implements ICmdLet, Runnable {
 		}
 	}
 
-	@Parameters(paramLabel = "<login@domain>", description = "login@domain of the user to create")
+	@Parameters(paramLabel = "<login@domain>", description = "login@domain of the user to create (if --count is specified, {} must be present in the login for the counter)")
 	public String loginAtDomain;
 
 	@Option(names = "--pass", description = "password to apply, otherwise localpart will be used")
@@ -80,6 +84,12 @@ public class UserQuickCreateCommand implements ICmdLet, Runnable {
 
 	@Option(names = "--random-datalocation", description = "Random datalocation attribution")
 	public Boolean randomDatalocation = false;
+
+	@Option(names = "--workers", description = "Number of threads to create users")
+	public int workers = 4;
+
+	@Option(names = "--count", description = "Number of users to create (default=1)")
+	public int usercount = 1;
 
 	private CliContext ctx;
 
@@ -102,71 +112,93 @@ public class UserQuickCreateCommand implements ICmdLet, Runnable {
 			ctx.error(loginAtDomain + " does not look like a valid email.");
 			return;
 		}
+		MQ.init().join();
+
 		String[] splitted = loginAtDomain.split("@");
 		String localPart = splitted[0];
-		String domainPart = splitted[1];
+		String domainName = splitted[1];
 
-		CliUtils cu = new CliUtils(ctx);
-		String dom = cu.getDomainUidByDomain(domainPart);
-		if (dom == null) {
-			ctx.error(domainPart + " is not a known domain or alias.");
-		}
-		IGroup grpApi = ctx.adminApi().instance(IGroup.class, dom);
-		ItemValue<Group> userGroup = grpApi.byName("user");
-		CountDownLatch cdl = new CountDownLatch(1);
-		MQ.init().thenAccept(v -> {
-			User u = new User();
-			u.login = localPart;
-			VCard card = new VCard();
-			String familyName = domainPart.toUpperCase();
-			String givenName = localPart;
-			String extraName = null;
-			String fn = givenName + " " + familyName;
-			List<Email> emails = new ArrayList<>();
-			Email defEmail = Email.create(loginAtDomain, true, false);
-			emails.add(defEmail);
-			if (Boolean.TRUE.equals(randomData)) {
-				familyName = nameFaker.lastName();
-				givenName = nameFaker.firstName();
-				extraName = localPart;
-				fn = givenName + " " + extraName + " " + familyName;
-				card.organizational.role = gotFaker.house();
-				card.communications.tels = Arrays.asList(Tel.create(phoneFaker.cellPhone(), Collections.emptyList()));
+		CliUtils cliUtils = new CliUtils(ctx);
+		String domainUid = cliUtils.getDomainUidByDomain(domainName);
 
-				String fakeLocal = forEmail(givenName) + "." + forEmail(familyName)
-						+ ThreadLocalRandom.current().nextInt(100, 1000);
-				defEmail.isDefault = false;
-				emails.add(Email.create(fakeLocal + "@" + domainPart, true, false));
-			}
-			card.identification.name = Name.create(familyName, givenName, extraName, null, null, null);
-			card.identification.formatedName = FormatedName.create(fn);
-			u.contactInfos = card;
+		IGroup groupApi = ctx.adminApi().instance(IGroup.class, domainUid);
 
-			u.password = Optional.ofNullable(password).orElse(localPart);
-			u.accountType = AccountType.FULL;
-			u.routing = Routing.internal;
-			u.emails = emails;
-
-			if (randomDatalocation == null || !randomDatalocation) {
-				u.dataLocation = ctx.adminApi().instance(IServer.class, "default").allComplete().stream()
-						.filter(s -> s.value.tags.contains("mail/imap")).findAny().map(s -> s.uid).orElse(null);
-			}
-
-			IUser uApi = ctx.adminApi().instance(IUser.class, dom);
-			String uid = "cli-created-" + UUID.nameUUIDFromBytes(loginAtDomain.getBytes()).toString().toLowerCase();
-			ctx.info("Creating " + uid + " for " + loginAtDomain);
-			uApi.create(uid, u);
-			if (userGroup != null) {
-				grpApi.add(userGroup.uid, Arrays.asList(Member.user(uid)));
-			}
-			cdl.countDown();
-		}).whenComplete((v, ex) -> {
-			if (ex != null) {
-				ctx.error(ex.getMessage());
+		if (usercount == 1) {
+			createUser(localPart, domainName, domainUid, groupApi);
+		} else {
+			if (!localPart.contains("{}")) {
+				ctx.error("NO: login@domain must contain '{}' for the counter");
 			} else {
-				ctx.info("finished for " + loginAtDomain);
+				ArrayBlockingQueue<String> q = new ArrayBlockingQueue<>(workers);
+				try (ExecutorService pool = Executors.newFixedThreadPool(workers)) {
+					for (int i = 0; i < usercount; i++) {
+						String login = MessageFormatter.format(localPart, i).getMessage();
+						try {
+							q.put(login);
+						} catch (InterruptedException ie) {
+							Thread.currentThread().interrupt();
+						}
+						pool.submit(() -> {
+							try {
+								createUser(login, domainName, domainUid, groupApi);
+							} finally {
+								q.remove(login); // NOSONAR
+							}
+						});
+					}
+				}
 			}
-		}).join();
+		}
+
+	}
+
+	private void createUser(String login, String domainName, String domainUid, IGroup groupApi) {
+		String latd = login + "@" + domainName;
+		User u = new User();
+		u.login = login;
+		VCard card = new VCard();
+		String familyName = domainName.toUpperCase();
+		String givenName = login;
+		String extraName = null;
+		String fn = givenName + " " + familyName;
+		List<Email> emails = new ArrayList<>();
+		Email defEmail = Email.create(latd, true, false);
+		emails.add(defEmail);
+		if (Boolean.TRUE.equals(randomData)) {
+			familyName = nameFaker.lastName();
+			givenName = nameFaker.firstName();
+			extraName = login;
+			fn = givenName + " " + extraName + " " + familyName;
+			card.organizational.role = gotFaker.house();
+			card.communications.tels = Arrays.asList(Tel.create(phoneFaker.cellPhone(), Collections.emptyList()));
+
+			String fakeLocal = forEmail(givenName) + "." + forEmail(familyName)
+					+ ThreadLocalRandom.current().nextInt(100, 1000);
+			defEmail.isDefault = false;
+			emails.add(Email.create(fakeLocal + "@" + domainName, true, false));
+		}
+		card.identification.name = Name.create(familyName, givenName, extraName, null, null, null);
+		card.identification.formatedName = FormatedName.create(fn);
+		u.contactInfos = card;
+
+		u.password = Optional.ofNullable(password).orElse(login);
+		u.accountType = AccountType.FULL;
+		u.routing = Routing.internal;
+		u.emails = emails;
+
+		if (randomDatalocation == null || !randomDatalocation) {
+			u.dataLocation = ctx.adminApi().instance(IServer.class, "default").allComplete().stream()
+					.filter(s -> s.value.tags.contains("mail/imap")).findAny().map(s -> s.uid).orElse(null);
+		}
+
+		IUser userApi = ctx.adminApi().instance(IUser.class, domainUid);
+		String uid = "cli-created-" + UUID.nameUUIDFromBytes(latd.getBytes()).toString().toLowerCase();
+		ctx.info("Creating {} for {}", uid, latd);
+		userApi.create(uid, u);
+		ItemValue<Group> userGroup = groupApi.byName("user");
+		if (userGroup != null) {
+			groupApi.add(userGroup.uid, Arrays.asList(Member.user(uid)));
+		}
 	}
 
 }
