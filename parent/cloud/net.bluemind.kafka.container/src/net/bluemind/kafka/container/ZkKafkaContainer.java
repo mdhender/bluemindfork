69541dd924570999c416b272156dc6e85dc9180b
Laurent Coustet <laurent.coustet@bluemind.net>
2024-03-22 19:15:40 +0100

    chore: fix kafka tests: target platform update for apache.commons.compress requiring apache.commons.io

--- parent/cloud/net.bluemind.kafka.container/src/net/bluemind/kafka/container/ZkKafkaContainer.java
+++ parent/cloud/net.bluemind.kafka.container/src/net/bluemind/kafka/container/ZkKafkaContainer.java
@@ -32,7 +32,7 @@ public class ZkKafkaContainer extends GenericContainer<ZkKafkaContainer> impleme
 	private static final Logger logger = LoggerFactory.getLogger(ZkKafkaContainer.class);
 
 	public ZkKafkaContainer() {
-		super("docker.bluemind.net/bluemind/zk-kafka:5.0.6181");
+		super("docker.bluemind.net/bluemind/zk-kafka:5.0.6980");
 		withExposedPorts(2181, port());
 		withReuse(false);
 		waitingFor(new org.testcontainers.containers.wait.strategy.AbstractWaitStrategy() {
