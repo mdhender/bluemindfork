3d8297bff59224cfd4817588370885bf0c8e7d66
Mathilde Michau <mathilde.michau@bluemind.net>
2024-05-03 15:28:52 +0200

    BM-20942 Fix: Display correct end date for full-days events

--- ui/vue-apps/mail-webapp/src/components/MailViewer/EventViewer/formatEventDates.js
+++ ui/vue-apps/mail-webapp/src/components/MailViewer/EventViewer/formatEventDates.js
@@ -1,10 +1,7 @@
 import i18n from "@bluemind/i18n";
 import { DateComparator } from "@bluemind/date";
 
-const ONE_DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
-
 const isWholeDayEvent = (dtstart, dtend) => dtstart.precision === "Date" && dtend.precision === "Date";
-const is24Hours = (start, end) => end.getTime() - start.getTime() === ONE_DAY_IN_MILLISECONDS;
 const getFormatedDate = (date, formats) => formats.map(format => i18n.d(date, format)).join(" ");
 
 const getFormatedDateRange = (start, end, formats) => ({
@@ -19,15 +16,18 @@ export function formatEventDates(dtstart, dtend, otherRelativeDates = []) {
 
     const isWholeDay = isWholeDayEvent(dtstart, dtend);
     const startDate = new Date(dtstart.iso8601);
-    const endDate = new Date(dtend.iso8601);
+    let endDate = new Date(dtend.iso8601);
+    if (isWholeDay) {
+        endDate.setDate(endDate.getDate() - 1); // Server end date is +1 day for whole days event
+    }
+    const isOneFullDay = isWholeDay && startDate.getDate() === endDate.getDate();
+
     const isNotSameDay = [dtend, ...otherRelativeDates]
         .filter(Boolean)
         .some(date => !DateComparator.isSameDay(startDate, new Date(date.iso8601)));
     const formats = [isNotSameDay ? "short_full_date" : undefined, !isWholeDay ? "short_time" : undefined].filter(
         Boolean
     );
-
-    const isOneFullDay = isWholeDay && is24Hours(startDate, endDate);
 
     return getFormatedDateRange(startDate, isOneFullDay ? undefined : endDate, formats);
 }
