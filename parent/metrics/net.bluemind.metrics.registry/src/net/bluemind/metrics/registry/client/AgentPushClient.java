cf75e805671db08b6c0f17cbc192bb858334570c
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2024-05-14 16:42:07 +0200

    Revert "[metrics] Chore: create json writer in constructor"
    This reverts commit 041178ed6bbc0ca572695ef6bdbd701a67f4fe34.

--- parent/metrics/net.bluemind.metrics.registry/src/net/bluemind/metrics/registry/client/AgentPushClient.java
+++ parent/metrics/net.bluemind.metrics.registry/src/net/bluemind/metrics/registry/client/AgentPushClient.java
@@ -7,26 +7,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectWriter;
 
 import net.bluemind.metrics.registry.impl.Mapper;
 import net.bluemind.metrics.registry.json.RegJson;
 
 public class AgentPushClient {
 
+	private ArrayBlockingQueue<byte[]> queue;
 	private static final Logger logger = LoggerFactory.getLogger(AgentPushClient.class);
-
-	private final ArrayBlockingQueue<byte[]> queue;
-	private final ObjectWriter writer;
 
 	public AgentPushClient(ArrayBlockingQueue<byte[]> sharedQueue) {
 		this.queue = sharedQueue;
-		this.writer = Mapper.get().writerFor(RegJson.class);
 	}
 
 	public void queue(RegJson dto) {
 		try {
-			byte[] chunk = writer.writeValueAsBytes(dto);
+			byte[] chunk = Mapper.get().writeValueAsBytes(dto);
 			boolean accepted = queue.offer(chunk, 1, TimeUnit.SECONDS);
 			while (!accepted) {
 				logger.warn("Blocking push to agent queue for {} byte(s)", chunk.length);
